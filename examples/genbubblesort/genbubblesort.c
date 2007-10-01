b7b35af437b2398dca353d5c7d35d8f5f547e78b
Robert Brummayer <brummayer@fmv2.(none)>
2007-10-01 15:40:56 +0200

    improved and added check that every element in the initial array is also in the sorted array

--- examples/genbubblesort/genbubblesort.c
+++ examples/genbubblesort/genbubblesort.c
@@ -21,23 +21,28 @@ int_to_bin (int x, char *buffer, int len)
 int
 main (int argc, char **argv)
 {
-  int num_bits       = 0;
-  int num_bits_index = 0;
-  int num_elements   = 0;
-  int i              = 0;
-  int j              = 0;
-  BtorExpMgr *emgr   = NULL;
-  BtorExp **indices  = NULL;
-  BtorExp *array     = NULL;
-  BtorExp *sgt       = NULL;
-  BtorExp *slte      = NULL;
-  BtorExp *temp      = NULL;
-  BtorExp *read1     = NULL;
-  BtorExp *read2     = NULL;
-  BtorExp *cond1     = NULL;
-  BtorExp *cond2     = NULL;
-  BtorExp *sorted    = NULL;
-  char *buffer       = NULL;
+  int num_bits             = 0;
+  int num_bits_index       = 0;
+  int num_elements         = 0;
+  int i                    = 0;
+  int j                    = 0;
+  BtorExpMgr *emgr         = NULL;
+  BtorExp **indices        = NULL;
+  BtorExp *array           = NULL;
+  BtorExp *ne              = NULL;
+  BtorExp *sgt             = NULL;
+  BtorExp *slte            = NULL;
+  BtorExp *temp            = NULL;
+  BtorExp *read1           = NULL;
+  BtorExp *read2           = NULL;
+  BtorExp *cond1           = NULL;
+  BtorExp *cond2           = NULL;
+  BtorExp *sorted          = NULL;
+  BtorExp *no_diff_element = NULL;
+  BtorExp *formula         = NULL;
+  BtorExp *index           = NULL;
+  BtorExp *old_element     = NULL;
+  char *buffer             = NULL;
   if (argc != 3)
   {
     printf ("Usage: ./genbubblesort <num-bits> <num-elements>\n");
@@ -70,6 +75,10 @@ main (int argc, char **argv)
     indices[i] = btor_const_exp (emgr, buffer);
   }
   array = btor_array_exp (emgr, num_bits, num_bits_index);
+  index = btor_var_exp (emgr, num_bits_index, "oldvalue");
+  /* read at an arbitrary index (needed later): */
+  old_element = btor_read_exp (emgr, array, index);
+  /* bubble sort algorithm */
   for (i = 1; i < num_elements; i++)
   {
     for (j = 0; j < num_elements - i; j++)
@@ -107,14 +116,40 @@ main (int argc, char **argv)
     btor_release_exp (emgr, read2);
     btor_release_exp (emgr, slte);
   }
+  /* we show that every element of the initial array
+   * occurs in the final sorted array by showing that
+   * there is no counter example:
+   * It is not the case that there exists an element in
+   * the initial array which does not occur in the sorted
+   * array.*/
+  no_diff_element = btor_const_exp (emgr, "1");
+  for (i = 0; i < num_elements; i++)
+  {
+    read1 = btor_read_exp (emgr, array, indices[i]);
+    ne    = btor_ne_exp (emgr, read1, old_element);
+    temp  = btor_and_exp (emgr, no_diff_element, ne);
+    btor_release_exp (emgr, no_diff_element);
+    no_diff_element = temp;
+    btor_release_exp (emgr, read1);
+    btor_release_exp (emgr, ne);
+  }
+  temp = btor_not_exp (emgr, no_diff_element);
+  btor_release_exp (emgr, no_diff_element);
+  no_diff_element = temp;
+  /* we conjunct this with the sorted predicate */
+  formula = btor_and_exp (emgr, sorted, no_diff_element);
   /* we negate the formula and show that it is unsatisfiable */
-  temp = btor_not_exp (emgr, sorted);
-  btor_release_exp (emgr, sorted);
-  sorted = temp;
-  btor_dump_exp (emgr, stdout, sorted);
+  temp = btor_not_exp (emgr, formula);
+  btor_release_exp (emgr, formula);
+  formula = temp;
+  btor_dump_exp (emgr, stdout, formula);
   /* clean up */
   for (i = 0; i < num_elements; i++) btor_release_exp (emgr, indices[i]);
+  btor_release_exp (emgr, formula);
   btor_release_exp (emgr, sorted);
+  btor_release_exp (emgr, no_diff_element);
+  btor_release_exp (emgr, old_element);
+  btor_release_exp (emgr, index);
   btor_release_exp (emgr, array);
   btor_delete_exp_mgr (emgr);
   free (buffer);
--- examples/genselectionsort/genselectionsort.c
+++ examples/genselectionsort/genselectionsort.c
@@ -21,24 +21,29 @@ int_to_bin (int x, char *buffer, int len)
 int
 main (int argc, char **argv)
 {
-  int num_bits         = 0;
-  int num_bits_index   = 0;
-  int num_elements     = 0;
-  int i                = 0;
-  int j                = 0;
-  BtorExpMgr *emgr     = NULL;
-  BtorExp **indices    = NULL;
-  BtorExp *array       = NULL;
-  BtorExp *min_index   = NULL;
-  BtorExp *min_element = NULL;
-  BtorExp *cur_element = NULL;
-  BtorExp *slt         = NULL;
-  BtorExp *slte        = NULL;
-  BtorExp *temp        = NULL;
-  BtorExp *read1       = NULL;
-  BtorExp *read2       = NULL;
-  BtorExp *sorted      = NULL;
-  char *buffer         = NULL;
+  int num_bits             = 0;
+  int num_bits_index       = 0;
+  int num_elements         = 0;
+  int i                    = 0;
+  int j                    = 0;
+  BtorExpMgr *emgr         = NULL;
+  BtorExp **indices        = NULL;
+  BtorExp *array           = NULL;
+  BtorExp *min_index       = NULL;
+  BtorExp *min_element     = NULL;
+  BtorExp *cur_element     = NULL;
+  BtorExp *old_element     = NULL;
+  BtorExp *index           = NULL;
+  BtorExp *ne              = NULL;
+  BtorExp *slt             = NULL;
+  BtorExp *slte            = NULL;
+  BtorExp *temp            = NULL;
+  BtorExp *read1           = NULL;
+  BtorExp *read2           = NULL;
+  BtorExp *no_diff_element = NULL;
+  BtorExp *sorted          = NULL;
+  BtorExp *formula         = NULL;
+  char *buffer             = NULL;
   if (argc != 3)
   {
     printf ("Usage: ./genselectionsort <num-bits> <num-elements>\n");
@@ -71,6 +76,10 @@ main (int argc, char **argv)
     indices[i] = btor_const_exp (emgr, buffer);
   }
   array = btor_array_exp (emgr, num_bits, num_bits_index);
+  index = btor_var_exp (emgr, num_bits_index, "oldvalue");
+  /* read at an arbitrary index (needed later): */
+  old_element = btor_read_exp (emgr, array, index);
+  /* selection sort algorithm */
   for (i = 0; i < num_elements - 1; i++)
   {
     min_element = btor_read_exp (emgr, array, indices[i]);
@@ -120,13 +129,39 @@ main (int argc, char **argv)
     btor_release_exp (emgr, read2);
     btor_release_exp (emgr, slte);
   }
+  /* we show that every element of the initial array
+   * occurs in the final sorted array by showing that
+   * there is no counter example:
+   * It is not the case that there exists an element in
+   * the initial array which does not occur in the sorted
+   * array.*/
+  no_diff_element = btor_const_exp (emgr, "1");
+  for (i = 0; i < num_elements; i++)
+  {
+    read1 = btor_read_exp (emgr, array, indices[i]);
+    ne    = btor_ne_exp (emgr, read1, old_element);
+    temp  = btor_and_exp (emgr, no_diff_element, ne);
+    btor_release_exp (emgr, no_diff_element);
+    no_diff_element = temp;
+    btor_release_exp (emgr, read1);
+    btor_release_exp (emgr, ne);
+  }
+  temp = btor_not_exp (emgr, no_diff_element);
+  btor_release_exp (emgr, no_diff_element);
+  no_diff_element = temp;
+  /* we conjunct this with the sorted predicate */
+  formula = btor_and_exp (emgr, sorted, no_diff_element);
   /* we negate the formula and show that it is unsatisfiable */
-  temp = btor_not_exp (emgr, sorted);
-  btor_release_exp (emgr, sorted);
-  sorted = temp;
-  btor_dump_exp (emgr, stdout, sorted);
+  temp = btor_not_exp (emgr, formula);
+  btor_release_exp (emgr, formula);
+  formula = temp;
+  btor_dump_exp (emgr, stdout, formula);
   /* clean up */
   for (i = 0; i < num_elements; i++) btor_release_exp (emgr, indices[i]);
+  btor_release_exp (emgr, old_element);
+  btor_release_exp (emgr, index);
+  btor_release_exp (emgr, formula);
+  btor_release_exp (emgr, no_diff_element);
   btor_release_exp (emgr, sorted);
   btor_release_exp (emgr, array);
   btor_delete_exp_mgr (emgr);
