a4d1c4fccba913146fb29e7882e0fce4cbfcb39a
Robert Brummayer <brummayer@fmv2.(none)>
2007-11-28 12:41:29 +0100

    fixed return codes

--- btorexit.h
+++ btorexit.h
@@ -3,11 +3,11 @@
 
 enum BtorExitCode
 {
-  BTOR_SUCC_EXIT,
-  BTOR_ERR_EXIT,
-  BTOR_SAT_EXIT,
-  BTOR_UNSAT_EXIT,
-  BTOR_UNKNOWN_EXIT
+  BTOR_SUCC_EXIT    = 0,
+  BTOR_ERR_EXIT     = 1,
+  BTOR_SAT_EXIT     = 2,
+  BTOR_UNSAT_EXIT   = 3,
+  BTOR_UNKNOWN_EXIT = 4
 };
 
 typedef enum BtorExitCode BtorExitCode;
