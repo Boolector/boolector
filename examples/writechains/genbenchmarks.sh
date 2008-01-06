be01f58c755fb28369ed488a29dd5e5e86d64ef4
Robert Brummayer <brummayer@fmv2.(none)>
2008-01-06 13:54:05 +0100

    improved

--- examples/writechains/genbenchmarks.sh
+++ examples/writechains/genbenchmarks.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
-for ((numbits=2;numbits<=9;numbits+=1))
+for ((a=0;a<=1;a+=1))
 do
-  header=1
-  filename=writechains$numbits".smt"
-  ./writechains $numbits | boolector -rwl0 -ds | while read line
+  inc=1
+  if [[ $a -eq 1 ]]; then
+    aarg="-a"
+    limit=100
+  else
+    aarg=""
+    limit=500
+  fi
+  for ((numbits=2;numbits<=$limit;numbits+=$inc))
   do
-    if [[ $header -eq 1 ]]; then
-      echo "(benchmark $filename" > $filename
-      echo ":source {" >> $filename
-      echo "This benchmark generates write chains and shows" >> $filename
-      echo "via extensionality that they are equal." >> $filename
-      echo "" >> $filename
-      echo -n "Contributed by Armin Biere " >> $filename
-      echo "(armin.biere@jku.at)." >> $filename
-      echo "}" >> $filename
-      echo ":status unsat" >> $filename
-      echo ":category { crafted }" >> $filename
-      header=0
+    header=1
+    if [[ $a -eq 1 ]]; then
+      filename=writechainsunsat$numbits".smt"
     else
-      echo $line >> $filename
+      filename=writechainssat$numbits".smt"
+    fi
+    ./writechains $aarg $numbits | boolector -rwl0 -ds | while read line
+    do
+      if [[ $header -eq 1 ]]; then
+	echo "(benchmark $filename" > $filename
+	echo ":source {" >> $filename
+	echo "This benchmark generates write chain permutations and tries to show" >> $filename
+	echo "via extensionality that they are equal." >> $filename
+	echo "" >> $filename
+	echo -n "Contributed by Armin Biere " >> $filename
+	echo "(armin.biere@jku.at)." >> $filename
+	echo "}" >> $filename
+	if [[ $a -eq 1 ]]; then
+	  echo ":status unsat" >> $filename
+	else
+	  echo ":status sat" >> $filename
+	fi
+	echo ":category { crafted }" >> $filename
+	header=0
+      else
+	echo $line >> $filename
+      fi
+    done
+    if [[ $numbits -eq 20 ]]; then
+      inc=2
+    elif [[ $numbits -eq 50 ]]; then
+      inc=5 
+    elif [[ $numbits -eq 100 ]]; then
+      inc=10
+    elif [[ $numbits -eq 200 ]]; then
+      inc=50
     fi
   done
 done
