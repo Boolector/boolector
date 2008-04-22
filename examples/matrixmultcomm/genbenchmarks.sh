83b2c19a1f92f1537c6912f9791c06ccc8c2f211
Robert Brummayer <brummayer@fmv2.(none)>
2008-04-22 08:33:38 +0200

    fixed

--- examples/matrixmultcomm/genbenchmarks.sh
+++ examples/matrixmultcomm/genbenchmarks.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
-for ((numbits=2;numbits<=8;numbits*=2))
+numbits=32
+for ((size=2;size<=12;size+=1))
 do
-  for ((size=2;size<=12;size+=1))
+  header=1
+  if [[ $size -lt 10 ]]; then
+    sizestring=0$size
+  else
+    sizestring=$size
+  fi
+  filename=matrixmultcomm$sizestring".smt"
+  ./matrixmultcomm $numbits $size | boolector -rwl0 -ds | while read line
   do
-    header=1
-    filename=matrixmultcomm$size"_"$numbits".smt"
-    ./matrixmultcomm $numbits $size | boolector -rwl0 -ds | while read line
-    do
-      if [[ $header -eq 1 ]]; then
-        echo "(benchmark $filename" > $filename
-	echo ":source {" >> $filename
-	echo "This benchmark verifies that matrix multiplication" >> $filename
-	echo "is not commutative in general." >> $filename
-	echo -n "The matrix has $size * $size fields of " >> $filename
-	echo "bit width $numbits." >> $filename
-	echo "It is modelled as one-dimensional array." >> $filename
-	echo "" >> $filename
-	echo -n "Contributed by Robert Brummayer " >> $filename
-	echo "(robert.brummayer@gmail.com)." >> $filename
-	echo "}" >> $filename
-	echo ":status sat" >> $filename
-	echo ":category { crafted }" >> $filename
-	header=0
-      else 
-        echo $line >> $filename
-      fi
-    done
+    if [[ $header -eq 1 ]]; then
+      echo "(benchmark $filename" > $filename
+      echo ":source {" >> $filename
+      echo -n "This benchmark shows that matrix multiplication" >> $filename
+      echo "is not commutative in general." >> $filename
+      echo "We try to show that A x B = B x A, which is generally not the case". >> $filename
+      echo -n "The matrices have $size * $size fields of " >> $filename
+      echo "bit-width $numbits," >> $filename
+      echo "and are respectively represented by a one-dimensional array." >> $filename
+      echo "" >> $filename
+      echo -n "Contributed by Robert Brummayer " >> $filename
+      echo "(robert.brummayer@gmail.com)." >> $filename
+      echo "}" >> $filename
+      echo ":status sat" >> $filename
+      echo ":category { crafted }" >> $filename
+      header=0
+    else 
+      echo $line >> $filename
+    fi
   done
 done
