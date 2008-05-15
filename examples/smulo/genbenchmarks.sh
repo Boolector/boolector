ca665a7bce9fa81bca11a8dd3d3fb4d23d5ae2f6
Robert Brummayer <brummayer@fmv2.(none)>
2008-05-15 18:15:27 +0200

    improved

--- examples/smulo/genbenchmarks.sh
+++ examples/smulo/genbenchmarks.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+for ((bits=8; bits <= 64; bits += 8))
+do
+  header=1
+  if [[ $bits -lt 10 ]]; then
+    bitsstring="0"$bits
+  else 
+    bitsstring=$bits
+  fi
+  filename=smulov1bw$bitsstring".smt"
+  ./smulov1 $bits | boolector -rwl0 -ds | while read line
+  do
+    if [[ $header -eq 1 ]]; then
+      echo "(benchmark $filename" > $filename
+      echo ":source {" >> $filename
+      echo "We verify the correctness of a signed multiplication" >> $filename
+      echo "overflow detection unit as proposed in" >> $filename
+      echo "\"Combined Unsigned and Two's Complement Saturating Multipliers\"" >> $filename
+      echo "by M. Schulte et al." >> $filename
+      echo "" >> $filename
+      echo "Bit-width: $bits" >> $filename
+      echo "" >> $filename
+      echo -n "Contributed by Robert Brummayer " >> $filename
+      echo "(robert.brummayer@gmail.com)." >> $filename
+      echo "}" >> $filename
+      echo ":status unsat" >> $filename
+      echo ":category { industrial }" >> $filename
+      header=0
+    else
+      echo $line >> $filename
+    fi
+  done
+  if [[ $bits -eq 128 ]]; then
+    inc=32
+  fi
+done
+
 inc=16
 for ((bits = 16; bits <= 512; bits += inc))
 do
