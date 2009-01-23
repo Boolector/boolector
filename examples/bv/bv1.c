5329abb30a5c32ad24547cacfc5cf023bdd2af3d
Robert Brummayer <brummayer@fmv2.(none)>
2009-01-23 13:15:08 +0100

    added API examples

---
+++ examples/bv/bv1.c
@@ -0,0 +1,74 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../boolector.h"
+
+#define BV1_EXAMPLE_NUM_BITS 8
+
+/* We verify the XOR swap algorithm. The XOR bitwise operation can
+ * be used to swap variables without using a temporary variable:
+ * int x, y;
+ * ...
+ * x = x ^ y
+ * y = x ^ y
+ * x = x ^ y
+ */
+
+int
+main (void)
+{
+  Btor *btor;
+  BtorExp *x, *y, *temp, *old_x, *old_y, *eq1, *eq2, *and, *formula;
+  int result;
+
+  btor = boolector_new ();
+  x    = boolector_var (btor, BV1_EXAMPLE_NUM_BITS, "v1");
+  y    = boolector_var (btor, BV1_EXAMPLE_NUM_BITS, "v2");
+  /* remember initial values of x and y */
+  old_x = boolector_copy (btor, x);
+  old_y = boolector_copy (btor, y);
+
+  /* x = x ^ y */
+  temp = boolector_xor (btor, x, y);
+  boolector_release (btor, x);
+  x = temp;
+
+  /* y = x ^ y */
+  temp = boolector_xor (btor, x, y);
+  boolector_release (btor, y);
+  y = temp;
+
+  /* x = x ^ y */
+  temp = boolector_xor (btor, x, y);
+  boolector_release (btor, x);
+  x = temp;
+
+  /* Now, we have to show that old_x = y and old_y = x */
+  eq1 = boolector_eq (btor, old_x, y);
+  eq2 = boolector_eq (btor, old_y, x);
+  and = boolector_and (btor, eq1, eq2);
+
+  /* In order to prove that this is a theorem, we negate the whole
+   * formula and show that the negation is unsatisfiable */
+  formula = boolector_not (btor, and);
+
+  /* We assert the formula and call Boolector */
+  boolector_assert (btor, formula);
+  result = boolector_sat (btor, INT_MAX);
+  if (result == BOOLECTOR_UNSAT)
+    printf ("Formula is unsatisfiable\n");
+  else
+    abort ();
+
+  /* cleanup */
+  boolector_release (btor, x);
+  boolector_release (btor, old_x);
+  boolector_release (btor, y);
+  boolector_release (btor, old_y);
+  boolector_release (btor, eq1);
+  boolector_release (btor, eq2);
+  boolector_release (btor, and);
+  boolector_release (btor, formula);
+  boolector_delete (btor);
+  return 0;
+}
---
+++ examples/bv/bv2.c
@@ -0,0 +1,93 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../boolector.h"
+
+#define BV2_EXAMPLE_NUM_BITS 8
+
+/* We try to show the following theorem:
+ * v1 > 0 & v2 > 0  =>  v1 + v2 > 0
+ *
+ * The theorem is valid if v1 and v2 are naturals, but not if they
+ * are two's complement bit-vectors as addition can overflow.
+ */
+
+int
+main (void)
+{
+  Btor *btor;
+  BtorExp *v1, *v2, *add, *zero, *vars_sgt_zero, *impl;
+  BtorExp *v1_sgt_zero, *v2_sgt_zero, *add_sgt_zero, *formula;
+  char *assignments[10];
+  int result, i;
+
+  btor = boolector_new ();
+  boolector_enable_model_gen (btor);
+
+  v1   = boolector_var (btor, BV2_EXAMPLE_NUM_BITS, "v1");
+  v2   = boolector_var (btor, BV2_EXAMPLE_NUM_BITS, "v2");
+  zero = boolector_zero (btor, BV2_EXAMPLE_NUM_BITS);
+
+  v1_sgt_zero   = boolector_sgt (btor, v1, zero);
+  v2_sgt_zero   = boolector_sgt (btor, v2, zero);
+  vars_sgt_zero = boolector_and (btor, v1_sgt_zero, v2_sgt_zero);
+
+  add          = boolector_add (btor, v1, v2);
+  add_sgt_zero = boolector_sgt (btor, add, zero);
+
+  impl = boolector_implies (btor, vars_sgt_zero, add_sgt_zero);
+
+  /* We negate the formula and try to show that the negation is unsatisfiable */
+  formula = boolector_not (btor, impl);
+
+  /* We assert the formula and call Boolector */
+  boolector_assert (btor, formula);
+  result = boolector_sat (btor, INT_MAX);
+  if (result == BOOLECTOR_SAT)
+    printf ("Instance is satisfiable");
+  else
+    abort ();
+
+  /* The formula is not valid, we have found a counter-example.
+   * Now, we are able to obtain assignments to arbitrary expressions */
+  i                = 0;
+  assignments[i++] = boolector_bv_assignment (btor, zero);
+  assignments[i++] = boolector_bv_assignment (btor, v1);
+  assignments[i++] = boolector_bv_assignment (btor, v2);
+  assignments[i++] = boolector_bv_assignment (btor, add);
+  assignments[i++] = boolector_bv_assignment (btor, v1_sgt_zero);
+  assignments[i++] = boolector_bv_assignment (btor, v2_sgt_zero);
+  assignments[i++] = boolector_bv_assignment (btor, vars_sgt_zero);
+  assignments[i++] = boolector_bv_assignment (btor, add_sgt_zero);
+  assignments[i++] = boolector_bv_assignment (btor, impl);
+  assignments[i++] = boolector_bv_assignment (btor, formula);
+
+  i = 0;
+  printf ("Assignment to 0: %s\n", assignments[i++]);
+  printf ("Assignment to v1: %s\n", assignments[i++]);
+  printf ("Assignment to v2: %s\n", assignments[i++]);
+  printf ("Assignment to v1 + v2: %s\n", assignments[i++]);
+  printf ("Assignment to v1 > 0: %s\n", assignments[i++]);
+  printf ("Assignment to v2 > 0: %s\n", assignments[i++]);
+  printf ("Assignment to v1 > 0 & v2 > 0: %s\n", assignments[i++]);
+  printf ("Assignment to v1 + v2 > 0: %s\n", assignments[i++]);
+  printf ("Assignment to v1 > 0 & v2 > 0  => v1 + v2 > 0: %s\n",
+          assignments[i++]);
+  printf ("Assignment to !(v1 > 0 & v2 > 0  => v1 + v2 > 0): %s\n",
+          assignments[i++]);
+  for (i = 0; i < 10; i++) boolector_free_bv_assignment (btor, assignments[i]);
+
+  /* cleanup */
+  boolector_release (btor, zero);
+  boolector_release (btor, v1);
+  boolector_release (btor, v2);
+  boolector_release (btor, add);
+  boolector_release (btor, impl);
+  boolector_release (btor, formula);
+  boolector_release (btor, v1_sgt_zero);
+  boolector_release (btor, v2_sgt_zero);
+  boolector_release (btor, vars_sgt_zero);
+  boolector_release (btor, add_sgt_zero);
+  boolector_delete (btor);
+  return 0;
+}
