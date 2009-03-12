49d71a194552dfc083c763f28052ea0227179049
Robert Brummayer <brummayer@fmv2.(none)>
2009-03-12 09:09:06 +0100

    added additional example

---
+++ examples/array/array3.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <limits.h>
+#include "../../boolector.h"
+
+#define ARRAY3_EXAMPLE_VALUE_BW 8
+#define ARRAY3_EXAMPLE_INDEX_BW 1
+
+int
+main ()
+{
+  int sat_result;
+  BtorExp *array, *index1, *index2, *read1, *read2, *eq, *ne;
+  Btor *btor;
+
+  btor = boolector_new ();
+  boolector_enable_inc_usage (btor);
+
+  array = boolector_array (
+      btor, ARRAY3_EXAMPLE_VALUE_BW, ARRAY3_EXAMPLE_INDEX_BW, NULL);
+  index1 = boolector_var (btor, ARRAY3_EXAMPLE_INDEX_BW, NULL);
+  index2 = boolector_var (btor, ARRAY3_EXAMPLE_INDEX_BW, NULL);
+  read1  = boolector_read (btor, array, index1);
+  read2  = boolector_read (btor, array, index2);
+  eq     = boolector_eq (btor, index1, index2);
+  ne     = boolector_ne (btor, read1, read2);
+
+  /* we enforce that index1 is equal to index 2 */
+  boolector_assert (btor, eq);
+  sat_result = boolector_sat (btor);
+  assert (sat_result == BTOR_SAT);
+  /* now we additionally assume that the read values differ
+   * the instance is now unsatasfiable as read congruence is violated */
+  boolector_assume (btor, ne);
+  sat_result = boolector_sat (btor);
+  assert (sat_result == BTOR_UNSAT);
+  /* after the SAT call the assumptions are gone
+   * the instance is now satisfiable again */
+  sat_result = boolector_sat (btor);
+  assert (sat_result == BTOR_SAT);
+  boolector_release (btor, array);
+  boolector_release (btor, index1);
+  boolector_release (btor, index2);
+  boolector_release (btor, read1);
+  boolector_release (btor, read2);
+  boolector_release (btor, eq);
+  boolector_release (btor, ne);
+  boolector_delete (btor);
+  return 0;
+}
