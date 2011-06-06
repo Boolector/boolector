fe4bf8321aa4994dfcb77f873856698ed243e21b
Armin Biere <armin.biere@gmail.com>
2011-06-06 15:57:26 +0200

    new version

--- srcrel/with-sat-solvers/build.sh
+++ srcrel/with-sat-solvers/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 for component in boolector picosat precosat lingeling
 do
   archive=`ls archives/${component}-*.tar.gz`
@@ -8,7 +9,17 @@ do
   mv $name $component
   echo "extracted $component"
 done
-echo "building picosat"
-cd picosat
-./configure -O >/dev/null || exit 1
+
+for component in picosat precosat lingeling
+do
+  echo "building $component"
+  cd $component
+  ./configure -O >/dev/null || exit 1
+  make >/dev/null || exit 1
+  cd ..
+done
+
+cd boolector
+./configure -precosat -lingeling >/dev/null || exit 1
 make >/dev/null || exit 1
+cd ..
