35db5b225fa9f69ed7128ceec454a55a100b558b
Armin Biere <armin.biere@gmail.com>
2011-07-04 02:14:16 +0200

    added btorsmt2.[ch]

---
+++ btorsmt2.h
@@ -0,0 +1,34 @@
+/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2011 Armin Biere, FMV, JKU.
+ *
+ *  Institute for Formal Models and Verification,
+ *  Johannes Kepler University, Linz, Austria.
+ *
+ *  This file is part of Boolector.
+ *
+ *  Boolector is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Boolector is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BTORSMT2_H_INLCUDED
+#define BTORSMT2_H_INLCUDED
+
+#include "btorexp.h"
+#include "btorparse.h"
+
+#include <stdio.h>
+
+const BtorParserAPI* btor_smt2_parser_api ();
+
+#endif
