149284ec562134f25ffec62034535c541d85027a
Armin Biere <armin.biere@gmail.com>
2012-06-07 11:40:15 +0200

    final

--- prepare-competition-zipfile.sh
+++ prepare-competition-zipfile.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+./configure -flto -static
+make boolector
+cp boolector run
+b=`./run -v /dev/null|grep ersion|grep oolector|awk '{print $(NF-1);exit}'`
+l=`./run -v /dev/null|grep ersion|grep ingeling|awk '{print $(NF-1);exit}'`
+version=boolector-${b}-${l}
+zipfile=${version}.zip
+rm -f $zipfile
+echo "--------------------------------------------------"
+echo zip $zipfile run
+zip $zipfile run
+ls -l $zipfile
