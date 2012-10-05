067b0ea32d5a514c1a5d2b7efd88779032aff2d7
Armin Biere <armin.biere@gmail.com>
2012-10-05 21:40:58 +0200

    simpdelay

--- btorsat.c
+++ btorsat.c
@@ -597,6 +597,7 @@ btor_passdown_lingeling_options (BtorSATMgr *smgr,
   return res;
 }
 
+#define BTOR_LGL_SIMP_DELAY 10000
 #define BTOR_LGL_MIN_BLIMIT 50000
 #define BTOR_LGL_MAX_BLIMIT 200000
 
@@ -638,6 +639,8 @@ btor_lingeling_sat (BtorSATMgr *smgr, int limit)
   int res, bfres;
   char name[80];
 
+  lglsetopt (lgl, "simpdelay", BTOR_LGL_SIMP_DELAY);
+
   if (!smgr->inc_required)
   {
     lglsetopt (lgl, "clim", -1);
@@ -656,10 +659,9 @@ btor_lingeling_sat (BtorSATMgr *smgr, int limit)
   {
     btor_msg_sat (smgr, 1, "blimit = %d", blgl->blimit);
     lglsetopt (lgl, "clim", blgl->blimit);
-    lglsetopt (lgl, "plain", 1);
     if (!(res = lglsat (lgl)))
     {
-      lglsetopt (lgl, "plain", 0);
+      lglsetopt (lgl, "simpdelay", 0);
       blgl->blimit *= 2;
       if (blgl->blimit > BTOR_LGL_MAX_BLIMIT)
         blgl->blimit = BTOR_LGL_MAX_BLIMIT;
