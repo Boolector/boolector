d80a69b8fcd865e1ea33820a7faee06b65f6d9e3
Aina Niemetz <aina.niemetz@gmail.com>
2012-11-19 09:31:25 +0100

    resolved merge conflict

--- tests/testlambda.c
+++ tests/testlambda.c
@@ -36,7 +36,7 @@ init_lambda_tests (void)
  *---------------------------------------------------------------------------*/
 
 static void
-test_const_lambda_const (void)
+test_lambda_const_lambda_const (void)
 {
   init_lambda_test ();
   BtorNode *param     = btor_param_exp (g_btor, g_index_bw, "p1");
@@ -60,7 +60,7 @@ test_const_lambda_const (void)
 }
 
 static void
-test_const_lambda_var (void)
+test_lambda_const_lambda_var (void)
 {
   init_lambda_test ();
   BtorNode *param   = btor_param_exp (g_btor, g_index_bw, "p1");
@@ -84,7 +84,7 @@ test_const_lambda_var (void)
 }
 
 static void
-test_const_lambda_param (void)
+test_lambda_const_lambda_param (void)
 {
   init_lambda_test ();
   BtorNode *param   = btor_param_exp (g_btor, g_elem_bw, "p1");
@@ -106,7 +106,7 @@ test_const_lambda_param (void)
 }
 
 static void
-test_unassigned_param (void)
+test_lambda_unassigned_param (void)
 {
   init_lambda_test ();
   BtorNode *param   = btor_param_exp (g_btor, g_index_bw, "p1");
@@ -155,37 +155,37 @@ unary_param_exp_test (BtorNode *(*func) (Btor *, BtorNode *) )
 }
 
 static void
-test_param_not (void)
+test_lambda_param_not (void)
 {
   unary_param_exp_test (btor_not_exp);
 }
 
 static void
-test_param_neg (void)
+test_lambda_param_neg (void)
 {
   unary_param_exp_test (btor_neg_exp);
 }
 
 static void
-test_param_redor (void)
+test_lambda_param_redor (void)
 {
   unary_param_exp_test (btor_redor_exp);
 }
 
 static void
-test_param_redxor (void)
+test_lambda_param_redxor (void)
 {
   unary_param_exp_test (btor_redxor_exp);
 }
 
 static void
-test_param_redand (void)
+test_lambda_param_redand (void)
 {
   unary_param_exp_test (btor_redand_exp);
 }
 
 static void
-test_param_slice (void)
+test_lambda_param_slice (void)
 {
   init_lambda_test ();
   int lower          = g_elem_bw / 2 + 1;
@@ -241,13 +241,13 @@ param_extension_test (BtorNode *(*func) (Btor *, BtorNode *, int) )
 }
 
 static void
-test_param_uext (void)
+test_lambda_param_uext (void)
 {
   param_extension_test (btor_uext_exp);
 }
 
 static void
-test_param_sext (void)
+test_lambda_param_sext (void)
 {
   param_extension_test (btor_sext_exp);
 }
@@ -285,241 +285,241 @@ binary_param_exp_test (BtorNode *(*func) (Btor *, BtorNode *, BtorNode *) )
 }
 
 static void
-test_param_implies (void)
+test_lambda_param_implies (void)
 {
   binary_param_exp_test (btor_implies_exp);
 }
 
 static void
-test_param_iff (void)
+test_lambda_param_iff (void)
 {
   binary_param_exp_test (btor_iff_exp);
 }
 
 static void
-test_param_xor (void)
+test_lambda_param_xor (void)
 {
   binary_param_exp_test (btor_xor_exp);
 }
 
 static void
-test_param_xnor (void)
+test_lambda_param_xnor (void)
 {
   binary_param_exp_test (btor_xnor_exp);
 }
 
 static void
-test_param_and (void)
+test_lambda_param_and (void)
 {
   binary_param_exp_test (btor_and_exp);
 }
 
 static void
-test_param_nand (void)
+test_lambda_param_nand (void)
 {
   binary_param_exp_test (btor_nand_exp);
 }
 
 static void
-test_param_or (void)
+test_lambda_param_or (void)
 {
   binary_param_exp_test (btor_or_exp);
 }
 
 static void
-test_param_nor (void)
+test_lambda_param_nor (void)
 {
   binary_param_exp_test (btor_nor_exp);
 }
 
 static void
-test_param_eq (void)
+test_lambda_param_eq (void)
 {
   binary_param_exp_test (btor_eq_exp);
 }
 
 static void
-test_param_ne (void)
+test_lambda_param_ne (void)
 {
   binary_param_exp_test (btor_ne_exp);
 }
 
 static void
-test_param_add (void)
+test_lambda_param_add (void)
 {
   binary_param_exp_test (btor_add_exp);
 }
 
 static void
-test_param_uaddo (void)
+test_lambda_param_uaddo (void)
 {
   binary_param_exp_test (btor_uaddo_exp);
 }
 
 static void
-test_param_saddo (void)
+test_lambda_param_saddo (void)
 {
   binary_param_exp_test (btor_saddo_exp);
 }
 
 static void
-test_param_mul (void)
+test_lambda_param_mul (void)
 {
   binary_param_exp_test (btor_mul_exp);
 }
 
 static void
-test_param_umulo (void)
+test_lambda_param_umulo (void)
 {
   binary_param_exp_test (btor_umulo_exp);
 }
 
 static void
-test_param_smulo (void)
+test_lambda_param_smulo (void)
 {
   binary_param_exp_test (btor_smulo_exp);
 }
 
 static void
-test_param_ult (void)
+test_lambda_param_ult (void)
 {
   binary_param_exp_test (btor_ult_exp);
 }
 
 static void
-test_param_slt (void)
+test_lambda_param_slt (void)
 {
   binary_param_exp_test (btor_slt_exp);
 }
 
 static void
-test_param_ulte (void)
+test_lambda_param_ulte (void)
 {
   binary_param_exp_test (btor_ulte_exp);
 }
 
 static void
-test_param_slte (void)
+test_lambda_param_slte (void)
 {
   binary_param_exp_test (btor_slte_exp);
 }
 
 static void
-test_param_ugt (void)
+test_lambda_param_ugt (void)
 {
   binary_param_exp_test (btor_ugt_exp);
 }
 
 static void
-test_param_sgt (void)
+test_lambda_param_sgt (void)
 {
   binary_param_exp_test (btor_sgt_exp);
 }
 
 static void
-test_param_ugte (void)
+test_lambda_param_ugte (void)
 {
   binary_param_exp_test (btor_ugte_exp);
 }
 
 static void
-test_param_sgte (void)
+test_lambda_param_sgte (void)
 {
   binary_param_exp_test (btor_sgte_exp);
 }
 
 static void
-test_param_sll (void)
+test_lambda_param_sll (void)
 {
   binary_param_exp_test (btor_sll_exp);
 }
 
 static void
-test_param_srl (void)
+test_lambda_param_srl (void)
 {
   binary_param_exp_test (btor_srl_exp);
 }
 
 static void
-test_param_sra (void)
+test_lambda_param_sra (void)
 {
   binary_param_exp_test (btor_sra_exp);
 }
 
 static void
-test_param_rol (void)
+test_lambda_param_rol (void)
 {
   binary_param_exp_test (btor_rol_exp);
 }
 
 static void
-test_param_ror (void)
+test_lambda_param_ror (void)
 {
   binary_param_exp_test (btor_ror_exp);
 }
 
 static void
-test_param_sub (void)
+test_lambda_param_sub (void)
 {
   binary_param_exp_test (btor_sub_exp);
 }
 
 static void
-test_param_usubo (void)
+test_lambda_param_usubo (void)
 {
   binary_param_exp_test (btor_usubo_exp);
 }
 
 static void
-test_param_ssubo (void)
+test_lambda_param_ssubo (void)
 {
   binary_param_exp_test (btor_ssubo_exp);
 }
 
 static void
-test_param_udiv (void)
+test_lambda_param_udiv (void)
 {
   binary_param_exp_test (btor_udiv_exp);
 }
 
 static void
-test_param_sdiv (void)
+test_lambda_param_sdiv (void)
 {
   binary_param_exp_test (btor_sdiv_exp);
 }
 
 static void
-test_param_sdivo (void)
+test_lambda_param_sdivo (void)
 {
   binary_param_exp_test (btor_sdivo_exp);
 }
 
 static void
-test_param_urem (void)
+test_lambda_param_urem (void)
 {
   binary_param_exp_test (btor_urem_exp);
 }
 
 static void
-test_param_srem (void)
+test_lambda_param_srem (void)
 {
   binary_param_exp_test (btor_srem_exp);
 }
 
 static void
-test_param_smod (void)
+test_lambda_param_smod (void)
 {
   binary_param_exp_test (btor_smod_exp);
 }
 
 static void
-test_param_concat (void)
+test_lambda_param_concat (void)
 {
   binary_param_exp_test (btor_concat_exp);
 }
 
 static void
-test_param_read (void)
+test_lambda_param_read (void)
 {
   init_lambda_test ();
   BtorNode *param    = btor_param_exp (g_btor, g_index_bw, "p1");
@@ -551,7 +551,7 @@ test_param_read (void)
  *---------------------------------------------------------------------------*/
 
 static void
-test_param_write1 (void)
+test_lambda_param_write1 (void)
 {
   init_lambda_test ();
   BtorNode *i         = btor_var_exp (g_btor, g_index_bw, "i");
@@ -581,7 +581,7 @@ test_param_write1 (void)
 }
 
 static void
-test_param_write2 (void)
+test_lambda_param_write2 (void)
 {
   init_lambda_test ();
   BtorNode *i         = btor_var_exp (g_btor, g_index_bw, "i");
@@ -611,7 +611,7 @@ test_param_write2 (void)
 }
 
 static void
-test_param_bcond (void)
+test_lambda_param_bcond (void)
 {
   init_lambda_test ();
   BtorNode *var       = btor_var_exp (g_btor, 1, "v1");
@@ -643,7 +643,7 @@ test_param_bcond (void)
 /* NOTE: right now, we have to use a read on the array condition as lambdas
  * return bit-vectors only. */
 static void
-test_param_acond (void)
+test_lambda_param_acond (void)
 {
   init_lambda_test ();
   BtorNode *var           = btor_var_exp (g_btor, g_index_bw, "v1");
@@ -690,7 +690,7 @@ test_param_acond (void)
  *---------------------------------------------------------------------------*/
 
 static void
-test_reduce_write1 (void)
+test_lambda_reduce_write1 (void)
 {
   init_lambda_test ();
   BtorNode *a     = btor_array_exp (g_btor, g_elem_bw, g_index_bw, "a");
@@ -722,7 +722,7 @@ test_reduce_write1 (void)
 }
 
 static void
-test_reduce_write2 (void)
+test_lambda_reduce_write2 (void)
 {
   init_lambda_test ();
   BtorNode *a        = btor_array_exp (g_btor, g_elem_bw, g_index_bw, "a");
@@ -756,7 +756,7 @@ test_reduce_write2 (void)
 }
 
 static void
-test_reduce_nested_writes (void)
+test_lambda_reduce_nested_writes (void)
 {
   init_lambda_test ();
   BtorNode *i = btor_var_exp (g_btor, g_index_bw, "i");
@@ -804,7 +804,7 @@ test_reduce_nested_writes (void)
 
 /* lambda x . (lambda y . (x + y)) */
 static void
-test_reduce_nested_lambdas_add1 (void)
+test_lambda_reduce_nested_lambdas_add1 (void)
 {
   init_lambda_test ();
   BtorNode *var1     = btor_var_exp (g_btor, g_elem_bw, "v1");
@@ -841,7 +841,7 @@ test_reduce_nested_lambdas_add1 (void)
 // FIXME: x + read (lambda) (else it's x + array)
 /* lambda x . (x + lambda y . y) */
 static void
-test_reduce_nested_lambdas_add2 (void)
+test_lambda_reduce_nested_lambdas_add2 (void)
 {
   init_lambda_test ();
   BtorNode *var1     = btor_var_exp (g_btor, g_elem_bw, "v1");
@@ -883,76 +883,76 @@ void
 run_lambda_tests (int argc, char **argv)
 {
   /* constant lambda tests */
-  BTOR_RUN_TEST (const_lambda_const);
-  BTOR_RUN_TEST (const_lambda_var);
-  BTOR_RUN_TEST (const_lambda_param);
-  BTOR_RUN_TEST (unassigned_param);
+  BTOR_RUN_TEST (lambda_const_lambda_const);
+  BTOR_RUN_TEST (lambda_const_lambda_var);
+  BTOR_RUN_TEST (lambda_const_lambda_param);
+  BTOR_RUN_TEST (lambda_unassigned_param);
   /* unary exp tests */
-  BTOR_RUN_TEST (param_not);
-  BTOR_RUN_TEST (param_neg);
-  BTOR_RUN_TEST (param_redor);
-  BTOR_RUN_TEST (param_redxor);
-  BTOR_RUN_TEST (param_redand);
-  BTOR_RUN_TEST (param_slice);
-  BTOR_RUN_TEST (param_uext);
-  BTOR_RUN_TEST (param_sext);
+  BTOR_RUN_TEST (lambda_param_not);
+  BTOR_RUN_TEST (lambda_param_neg);
+  BTOR_RUN_TEST (lambda_param_redor);
+  BTOR_RUN_TEST (lambda_param_redxor);
+  BTOR_RUN_TEST (lambda_param_redand);
+  BTOR_RUN_TEST (lambda_param_slice);
+  BTOR_RUN_TEST (lambda_param_uext);
+  BTOR_RUN_TEST (lambda_param_sext);
   /* binary exp tests */
-  BTOR_RUN_TEST (param_implies);
-  BTOR_RUN_TEST (param_iff);
-  BTOR_RUN_TEST (param_xor);
-  BTOR_RUN_TEST (param_xnor);
-  BTOR_RUN_TEST (param_and);
-  BTOR_RUN_TEST (param_nand);
-  BTOR_RUN_TEST (param_or);
-  BTOR_RUN_TEST (param_nor);
-  BTOR_RUN_TEST (param_eq);
-  BTOR_RUN_TEST (param_ne);
-  BTOR_RUN_TEST (param_add);
-  BTOR_RUN_TEST (param_uaddo);
-  BTOR_RUN_TEST (param_saddo);
-  BTOR_RUN_TEST (param_mul);
-  BTOR_RUN_TEST (param_umulo);
-  BTOR_RUN_TEST (param_smulo);
-  BTOR_RUN_TEST (param_ult);
-  BTOR_RUN_TEST (param_slt);
-  BTOR_RUN_TEST (param_ulte);
-  BTOR_RUN_TEST (param_slte);
-  BTOR_RUN_TEST (param_ugt);
-  BTOR_RUN_TEST (param_sgt);
-  BTOR_RUN_TEST (param_ugte);
-  BTOR_RUN_TEST (param_sgte);
-  BTOR_RUN_TEST (param_sll);
-  BTOR_RUN_TEST (param_srl);
-  BTOR_RUN_TEST (param_sra);
-  BTOR_RUN_TEST (param_rol);
-  BTOR_RUN_TEST (param_ror);
-  BTOR_RUN_TEST (param_sub);
-  BTOR_RUN_TEST (param_usubo);
-  BTOR_RUN_TEST (param_ssubo);
-  BTOR_RUN_TEST (param_udiv);
-  BTOR_RUN_TEST (param_sdiv);
-  BTOR_RUN_TEST (param_sdivo);
-  BTOR_RUN_TEST (param_urem);
-  BTOR_RUN_TEST (param_srem);
-  BTOR_RUN_TEST (param_smod);
-  BTOR_RUN_TEST (param_concat);
-  BTOR_RUN_TEST (param_read);
+  BTOR_RUN_TEST (lambda_param_implies);
+  BTOR_RUN_TEST (lambda_param_iff);
+  BTOR_RUN_TEST (lambda_param_xor);
+  BTOR_RUN_TEST (lambda_param_xnor);
+  BTOR_RUN_TEST (lambda_param_and);
+  BTOR_RUN_TEST (lambda_param_nand);
+  BTOR_RUN_TEST (lambda_param_or);
+  BTOR_RUN_TEST (lambda_param_nor);
+  BTOR_RUN_TEST (lambda_param_eq);
+  BTOR_RUN_TEST (lambda_param_ne);
+  BTOR_RUN_TEST (lambda_param_add);
+  BTOR_RUN_TEST (lambda_param_uaddo);
+  BTOR_RUN_TEST (lambda_param_saddo);
+  BTOR_RUN_TEST (lambda_param_mul);
+  BTOR_RUN_TEST (lambda_param_umulo);
+  BTOR_RUN_TEST (lambda_param_smulo);
+  BTOR_RUN_TEST (lambda_param_ult);
+  BTOR_RUN_TEST (lambda_param_slt);
+  BTOR_RUN_TEST (lambda_param_ulte);
+  BTOR_RUN_TEST (lambda_param_slte);
+  BTOR_RUN_TEST (lambda_param_ugt);
+  BTOR_RUN_TEST (lambda_param_sgt);
+  BTOR_RUN_TEST (lambda_param_ugte);
+  BTOR_RUN_TEST (lambda_param_sgte);
+  BTOR_RUN_TEST (lambda_param_sll);
+  BTOR_RUN_TEST (lambda_param_srl);
+  BTOR_RUN_TEST (lambda_param_sra);
+  BTOR_RUN_TEST (lambda_param_rol);
+  BTOR_RUN_TEST (lambda_param_ror);
+  BTOR_RUN_TEST (lambda_param_sub);
+  BTOR_RUN_TEST (lambda_param_usubo);
+  BTOR_RUN_TEST (lambda_param_ssubo);
+  BTOR_RUN_TEST (lambda_param_udiv);
+  BTOR_RUN_TEST (lambda_param_sdiv);
+  BTOR_RUN_TEST (lambda_param_sdivo);
+  BTOR_RUN_TEST (lambda_param_urem);
+  BTOR_RUN_TEST (lambda_param_srem);
+  BTOR_RUN_TEST (lambda_param_smod);
+  BTOR_RUN_TEST (lambda_param_concat);
+  BTOR_RUN_TEST (lambda_param_read);
   /* ternary exp tests */
-  BTOR_RUN_TEST (param_write1);
-  BTOR_RUN_TEST (param_write2);
-  BTOR_RUN_TEST (param_bcond);
-  BTOR_RUN_TEST (param_acond);
+  BTOR_RUN_TEST (lambda_param_write1);
+  BTOR_RUN_TEST (lambda_param_write2);
+  BTOR_RUN_TEST (lambda_param_bcond);
+  BTOR_RUN_TEST (lambda_param_acond);
 
   /* full reduction tests (with reduced expressions) */
-  BTOR_RUN_TEST (reduce_write1);
-  BTOR_RUN_TEST (reduce_write2);
-  BTOR_RUN_TEST (reduce_nested_writes);
+  BTOR_RUN_TEST (lambda_reduce_write1);
+  BTOR_RUN_TEST (lambda_reduce_write2);
+  BTOR_RUN_TEST (lambda_reduce_nested_writes);
 
   /* partial reduction tests */
 
   /* additional tests */
-  BTOR_RUN_TEST (reduce_nested_lambdas_add1);
-  // FIXME: BTOR_RUN_TEST (reduce_nested_lambdas_add2);
+  BTOR_RUN_TEST (lambda_reduce_nested_lambdas_add1);
+  //  BTOR_RUN_TEST (lambda_reduce_nested_lambdas_add2);
 }
 
 void
