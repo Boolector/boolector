4cfc73797f32e5270e8e5c35589375fce6777a65
Armin Biere <armin.biere@gmail.com>
2012-12-29 22:01:26 +0100

    finally figured how to trigger test recompilation when switchting branches

--- test.mk
+++ test.mk
@@ -1,14 +1,24 @@
 TESTDIR=tests
-TESTOBJ=$(addsuffix .o,$(basename $(wildcard $(TESTDIR)/test*.c )))
-SRC+=$(shell ls $(TESTDIR)/*.c)
+TESTSRC=$(wildcard $(TESTDIR)/test*.c )
+TESTOBJ=$(addsuffix .o,$(basename $(TESTSRC)))
 CFLAGS+=-I"$(shell pwd)"
 
 all: test
 
+-include test.dep
+
 clean: test-clean
+
 test-clean:
 	rm -f $(TESTDIR)/*.o
 	rm -f test.dep
+
+test.dep: btorconfig.h $(SRC) $(TESTSRC) makefile test.mk
+	rm -f $@; \
+	$(CC) $(CFLAGS) -MM $(TESTSRC) -I"$(shell pwd)"| \
+	sed -e 's,:,: $(shell pwd)/makefile,' \
+	    -e 's,^test,tests/test,' \
+	    >$@
 
 test: $(TESTOBJ) libboolector.a  $(LDEPS)
 	$(CC) $(CFLAGS) -o $@ $(TESTOBJ) -L. -lboolector $(LIBS)
