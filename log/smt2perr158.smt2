fd2792ee099df1070e4f7ba5e809c76fb82bd218
Armin Biere <armin.biere@gmail.com>
2012-12-29 21:32:22 +0100

    factored test.mk makefile (again)

---
+++ log/smt2perr158.smt2
@@ -0,0 +1,2 @@
+(assert
+(bvneg bvurem)
---
+++ test.mk
@@ -0,0 +1,13 @@
+TESTDIR=tests
+TESTOBJ=$(addsuffix .o,$(basename $(wildcard $(TESTDIR)/test*.c )))
+SRC+=$(shell ls $(TESTDIR)/*.c)
+CFLAGS+=-I"$(shell pwd)"
+
+all: test
+
+clean:
+	rm -f $(TESTDIR)/*.o
+	rm -f test.dep
+
+test: $(TESTOBJ) libboolector.a  $(LDEPS)
+	$(CC) $(CFLAGS) -o $@ $(TESTOBJ) -L. -lboolector $(LIBS)
