55907bc799b2c04d6c9786b322ffedadc117f0f7
Mathias Preiner <mathias.preiner@gmail.com>
2013-03-26 19:25:45 +0100

    btorstats: some more stats

--- contrib/btorstats.py
+++ contrib/btorstats.py
@@ -2,6 +2,51 @@
 
 import sys
 
+#def find_dominated_conds():
+#    global num_dominated_conds
+#
+#    for cond in conds:
+#        visit = [cond]
+#        visited = {}
+#        if node_ref[cond] > 1:
+#            continue
+#
+#        while len(visit) > 0:
+#            cur = visit.pop()
+#            if cur in visited:
+#                continue
+#
+#            visited[cur] = 1
+#            if cur in node_parents:
+#                for parent in node_parents[cur]:
+#                    visit.append(parent)
+#                    if parent in conds:
+#                        if node_children[parent][0] == node_children[cond][0]:
+#                            num_dominated_conds += 1
+#
+#def find_dominated_aconds():
+#    global num_dominated_aconds
+#
+#    for acond in aconds:
+#        visit = [acond]
+#        visited = {}
+#        if node_ref[acond] > 1:
+#            continue
+#
+#        while len(visit) > 0:
+#            cur = visit.pop()
+#            if cur in visited:
+#                continue
+#
+#            visited[cur] = 1
+#            if cur in node_parents:
+#                for parent in node_parents[cur]:
+#                    visit.append(parent)
+#                    if parent in aconds:
+#                        if node_children[parent][0] == node_children[acond][0]:
+#                            num_dominated_aconds += 1
+
+            
 if __name__ == "__main__":
     infile = sys.stdin
 
@@ -17,14 +62,23 @@ if __name__ == "__main__":
     nodes_cnt = {}
     nodes_ref = {}
     node_ref = {}
+    node_parents = {}
+    node_children = {}
 
     writes = []
     aconds = []
+    conds = []
     arrays = []
     lambdas = []
     params = []
+    roots = []
 
+    num_dominated_aconds = 0
+    num_dominated_conds = 0
     num_total_nodes = 0
+    num_const_index_reads = 0
+    num_const_index_writes = 0
+    num_const_value_writes = 0
     reads_on_arrays = 0
     reads_on_aconds = 0
     reads_on_writes = 0
@@ -56,6 +110,8 @@ if __name__ == "__main__":
             children = [abs(int(s)) for s in tokens[3:]]
         else:
             children = []
+
+        node_children[id] = children
         
         if symbol not in nodes_ref:
             nodes_ref[symbol] = 0
@@ -65,6 +121,9 @@ if __name__ == "__main__":
         for cid in children:
             nodes_ref[id2symbol[cid]] += 1
             node_ref[cid] += 1
+            if not cid in node_parents:
+                node_parents[cid] = []
+            node_parents[cid].append(id)
 
         if symbol not in nodes_cnt:
             nodes_cnt[symbol] = 0
@@ -72,6 +131,9 @@ if __name__ == "__main__":
 
 
         if symbol == "read":
+            if id2symbol[children[1]] == "const":
+                num_const_index_reads += 1
+
             if children[0] in aconds:
                 reads_on_aconds += 1
             elif children[0] in arrays: 
@@ -88,6 +150,11 @@ if __name__ == "__main__":
                 reads_on_lambda_node[children[0]] += 1
             else:
                 print(children[0])
+        elif symbol == "write":
+            if id2symbol[children[1]] == "const":
+                num_const_index_writes += 1
+            if id2symbol[children[2]] == "const":
+                num_const_value_writes += 1
 
         if symbol == "write":
             writes.append(id)
@@ -99,10 +166,18 @@ if __name__ == "__main__":
             lambdas.append(id)
         elif symbol == "param":
             params.append(id)
+        elif symbol == "cond":
+            conds.append(id)
+        elif symbol == "root":
+            roots.append(id)
     
 
     if infile != sys.stdin:
         infile.close()
+
+    
+#    find_dominated_conds()
+#    find_dominated_aconds()
 
 
     writes_with_one_ref = 0
@@ -122,6 +197,9 @@ if __name__ == "__main__":
     print("{0:8s} {1:5d}".format("total:", num_total_nodes))
 
     print("")
+    print("writes with constant indices: {}".format(num_const_index_writes))
+    print("writes with constant values: {}".format(num_const_value_writes))
+    print("reads with constant indices: {}".format(num_const_index_reads))
     print("reads on aconds: {0:d}".format(reads_on_aconds))
     print("reads on arrays: {0:d}".format(reads_on_arrays))
     # top 5 writes with most read references
@@ -134,4 +212,6 @@ if __name__ == "__main__":
 
     print("writes with one ref: {0:d}".format(writes_with_one_ref))
     print("lambdas with one ref: {0:d}".format(lambdas_with_one_ref))
+#    print("dominated aconds: {}".format(num_dominated_aconds))
+#    print("dominated conds: {}".format(num_dominated_conds))
 
