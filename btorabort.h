558fb9e0c3ddef482dcbce9154996afc3d92aa4d
Armin Biere <armin.biere@gmail.com>
2013-04-01 14:09:59 +0200

    abort to stderr

--- btorabort.h
+++ btorabort.h
@@ -19,63 +19,63 @@
 
 /*------------------------------------------------------------------------*/
 
-#define BTOR_ABORT_BOOLECTOR(cond, fmt, msg...)                     \
-  do                                                                \
-  {                                                                 \
-    if (cond)                                                       \
-    {                                                               \
-      printf ("[%s] %s: " fmt "\n", __FILE__, __FUNCTION__, ##msg); \
-      fflush (stdout);                                              \
-      abort ();                                                     \
-    }                                                               \
+#define BTOR_ABORT_BOOLECTOR(cond, fmt, msg...)                              \
+  do                                                                         \
+  {                                                                          \
+    if (cond)                                                                \
+    {                                                                        \
+      fprintf (stderr, "[%s] %s: " fmt "\n", __FILE__, __FUNCTION__, ##msg); \
+      fflush (stderr);                                                       \
+      abort ();                                                              \
+    }                                                                        \
   } while (0)
 
-#define BTOR_ABORT_ARG_NULL_BOOLECTOR(arg)          \
-  do                                                \
-  {                                                 \
-    if ((arg) == NULL)                              \
-    {                                               \
-      printf ("[%s] %s: ", __FILE__, __FUNCTION__); \
-      printf ("'%s' must not be NULL\n", #arg);     \
-      fflush (stdout);                              \
-      abort ();                                     \
-    }                                               \
+#define BTOR_ABORT_ARG_NULL_BOOLECTOR(arg)                   \
+  do                                                         \
+  {                                                          \
+    if ((arg) == NULL)                                       \
+    {                                                        \
+      fprintf (stderr, "[%s] %s: ", __FILE__, __FUNCTION__); \
+      fprintf (stderr, "'%s' must not be NULL\n", #arg);     \
+      fflush (stderr);                                       \
+      abort ();                                              \
+    }                                                        \
   } while (0)
 
-#define BTOR_ABORT_REFS_NOT_POS_BOOLECTOR(arg)                      \
-  do                                                                \
-  {                                                                 \
-    if (BTOR_REAL_ADDR_NODE ((arg))->refs < 1)                      \
-    {                                                               \
-      printf ("[%s] %s: ", __FILE__, __FUNCTION__);                 \
-      printf ("reference counter of '%s' must not be < 1\n", #arg); \
-      fflush (stdout);                                              \
-      abort ();                                                     \
-    }                                                               \
+#define BTOR_ABORT_REFS_NOT_POS_BOOLECTOR(arg)                               \
+  do                                                                         \
+  {                                                                          \
+    if (BTOR_REAL_ADDR_NODE ((arg))->refs < 1)                               \
+    {                                                                        \
+      fprintf (stderr, "[%s] %s: ", __FILE__, __FUNCTION__);                 \
+      fprintf (stderr, "reference counter of '%s' must not be < 1\n", #arg); \
+      fflush (stderr);                                                       \
+      abort ();                                                              \
+    }                                                                        \
   } while (0)
 
-#define BTOR_ABORT_ARRAY_BOOLECTOR(arg)                   \
-  do                                                      \
-  {                                                       \
-    if (BTOR_IS_ARRAY_NODE (BTOR_REAL_ADDR_NODE ((arg)))) \
-    {                                                     \
-      printf ("[%s] %s: ", __FILE__, __FUNCTION__);       \
-      printf ("'%s' must not be an array\n", #arg);       \
-      fflush (stdout);                                    \
-      abort ();                                           \
-    }                                                     \
+#define BTOR_ABORT_ARRAY_BOOLECTOR(arg)                      \
+  do                                                         \
+  {                                                          \
+    if (BTOR_IS_ARRAY_NODE (BTOR_REAL_ADDR_NODE ((arg))))    \
+    {                                                        \
+      fprintf (stderr, "[%s] %s: ", __FILE__, __FUNCTION__); \
+      fprintf (stderr, "'%s' must not be an array\n", #arg); \
+      fflush (stderr);                                       \
+      abort ();                                              \
+    }                                                        \
   } while (0)
 
-#define BTOR_ABORT_BV_BOOLECTOR(arg)                       \
-  do                                                       \
-  {                                                        \
-    if (!BTOR_IS_ARRAY_NODE (BTOR_REAL_ADDR_NODE ((arg)))) \
-    {                                                      \
-      printf ("[%s] %s: ", __FILE__, __FUNCTION__);        \
-      printf ("'%s' must not be a bit-vector\n", #arg);    \
-      fflush (stdout);                                     \
-      abort ();                                            \
-    }                                                      \
+#define BTOR_ABORT_BV_BOOLECTOR(arg)                             \
+  do                                                             \
+  {                                                              \
+    if (!BTOR_IS_ARRAY_NODE (BTOR_REAL_ADDR_NODE ((arg))))       \
+    {                                                            \
+      fprintf (stderr, "[%s] %s: ", __FILE__, __FUNCTION__);     \
+      fprintf (stderr, "'%s' must not be a bit-vector\n", #arg); \
+      fflush (stderr);                                           \
+      abort ();                                                  \
+    }                                                            \
   } while (0)
 
 #define BTOR_ABORT_NE_BW(arg1, arg2)                                         \
