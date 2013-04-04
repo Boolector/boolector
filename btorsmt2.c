faa64c60ebc333ea8cb58ed606abb64c55efa1d8
Mathias Preiner <mathias.preiner@gmail.com>
2013-04-04 13:54:32 +0200

    updated copyright

--- btorsmt2.c
+++ btorsmt2.c
@@ -1,7 +1,7 @@
 /*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
  *
  *  Copyright (C) 2011-2013 Armin Biere.
- *  Copyright (C) 2013 Aina Niemetz.
+ *  Copyright (C) 2013 Aina Niemetz, Mathias Preiner.
  *
  *  All rights reserved.
  *
