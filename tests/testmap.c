8b5b79e3576babc454ed9d24436da63c3f6d8f5c
Armin Biere <armin.biere@gmail.com>
2013-04-14 09:21:24 +0200

    second map test working

--- tests/testmap.c
+++ tests/testmap.c
@@ -66,7 +66,39 @@ test_mapnewdel ()
 /*------------------------------------------------------------------------*/
 
 void
+test_map0 ()
+{
+  init_map_test ();
+  {
+    BtorNode *a = boolector_var (g_btor, 32, "a");
+    BtorNode *b = boolector_var (g_btor, 32, "b");
+    BtorNode *s = boolector_sub (g_btor, a, b);
+    BtorNode *o = boolector_one (g_btor, 32);
+    BtorNode *t = boolector_unsigned_int (g_btor, 2, 32);
+    {
+      BtorNodeMap *map = btor_new_node_map (g_btor);
+      BtorNode *d;
+      btor_map_node (g_btor, map, a, t);
+      btor_map_node (g_btor, map, b, o);
+      d = btor_non_recursive_substitute_node (g_btor, map, s);
+      assert (d == o);
+      btor_delete_node_map (g_btor, map);
+      // boolector_release (g_btor, d); // No, map owns reference!!!!!!
+    }
+    boolector_release (g_btor, a);
+    boolector_release (g_btor, b);
+    boolector_release (g_btor, o);
+    boolector_release (g_btor, s);
+    boolector_release (g_btor, t);
+  }
+  finish_map_test ();
+}
+
+/*------------------------------------------------------------------------*/
+
+void
 run_map_tests (int argc, char **argv)
 {
   BTOR_RUN_TEST (mapnewdel);
+  BTOR_RUN_TEST (map0);
 }
