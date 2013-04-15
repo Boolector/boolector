610752ad1e4e05bc2a620a8654f1c3448586704a
Mathias Preiner <mathias.preiner@gmail.com>
2013-04-15 15:33:49 +0200

    btormisc: moved btor_vis_exp to btormisc
    further, added copyright

--- btormisc.c
+++ btormisc.c
@@ -1,4 +1,17 @@
+/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2007-2009 Robert Daniel Brummayer.
+ *  Copyright (C) 2007-2013 Armin Biere.
+ *  Copyright (C) 2012-2013 Aina Niemetz, Mathias Preiner.
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of Boolector.
+ *  See COPYING for more information on using this software.
+ */
+
 #include "btormisc.h"
+#include "btordump.h"
 
 char g_strbuf[256];
 int g_strbufpos = 0;
@@ -78,4 +91,21 @@ node2string (BtorNode *exp)
   g_strbufpos += len;
 
   return bufstart;
+}
+
+int
+btor_vis_exp (Btor *btor, BtorNode *exp)
+{
+  char cmd[100], *path;
+  FILE *file;
+  int res;
+  sprintf (cmd, "btorvis --ids ");
+  path = cmd + strlen (cmd);
+  sprintf (path, "/tmp/btorvisexp.%d.btor", btor->vis_idx++);
+  file = fopen (path, "w");
+  btor_dump_exp (btor, file, exp);
+  fclose (file);
+  strcat (cmd, "&");
+  res = system (cmd);
+  return res;
 }
--- btormisc.h
+++ btormisc.h
@@ -1,8 +1,21 @@
+/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2007-2009 Robert Daniel Brummayer.
+ *  Copyright (C) 2007-2013 Armin Biere.
+ *  Copyright (C) 2012-2013 Aina Niemetz, Mathias Preiner.
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of Boolector.
+ *  See COPYING for more information on using this software.
+ */
+
 #ifndef BTORMISC_H_INCLUDED
 #define BTORMISC_H_INCLUDED
 
 #include "btorexp.h"
 
 char *node2string (BtorNode *);
+int btor_vis_exp (Btor *btor, BtorNode *exp);
 
 #endif
