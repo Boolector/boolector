33cc65bccad481d7bbd4d25b392d0b8ed2943ccb
Armin Biere <armin.biere@gmail.com>
2013-05-08 13:37:54 +0200

    added btorimc.cc

---
+++ btorimc.cc
@@ -0,0 +1,9 @@
+#include "boolector.h"
+#include "btoribv.h"
+#include "btormc.h"
+
+int
+main (int argc, char** argv)
+{
+  return 0;
+}
--- ibv.mk
+++ ibv.mk
@@ -1,1 +1,7 @@
-all: btoribv.o
+LIBOBJS+=btoribv.o
+all: btorimc
+btorimc: btorimc.o libboolector.a $(LDEPS)
+	$(CC) $(CFLAGS) -o $@ btorimc.o -L. -lboolector $(LIBS)
+clean: ibv.clean
+ibv.clean:
+	rm -f btorimc
