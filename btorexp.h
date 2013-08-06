1487679ebc65c1f2ff006ba1dea0c0c599491401
Mathias Preiner <mathias.preiner@gmail.com>
2013-08-06 15:49:38 +0200

    set beta_mark flag to 2 bit

--- btorexp.h
+++ btorexp.h
@@ -149,7 +149,7 @@ typedef struct BtorNodePair BtorNodePair;
     unsigned int mark : 3;       /* for DAG traversal */                \
     unsigned int aux_mark : 2;   /* auxiliary mark flag */              \
     unsigned int fun_mark : 1;   /* for bottom up function traversal */ \
-    unsigned int beta_mark : 1;  /* mark for beta_reduce */             \
+    unsigned int beta_mark : 2;  /* mark for beta_reduce */             \
     unsigned int eval_mark : 2;  /* mark for eval_exp */                \
     unsigned int synth_mark : 2; /* mark for synthesize_exp */          \
     unsigned int reachable : 1;  /* reachable from root ? */            \
