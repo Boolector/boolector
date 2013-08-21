ece4406871efa0806949584ec2e633374bb846f1
Mathias Preiner <mathias.preiner@gmail.com>
2013-08-21 18:52:37 +0200

    boolector_apply: print api trace before sort check

--- boolector.c
+++ boolector.c
@@ -1644,12 +1644,6 @@ boolector_apply (Btor *btor, int argc, BtorNode **args, BtorNode *fun)
   char *strtrapi;
   BtorNode *res;
 
-  i = btor_fun_sort_check (btor, argc, args, fun);
-  BTOR_ABORT_BOOLECTOR (i >= 0,
-                        "sort of argument at position %d does not match given"
-                        " function signature",
-                        i);
-
   len = 7 + 10 + argc * 20 + 20;
   BTOR_NEWN (btor->mm, strtrapi, len);
   sprintf (strtrapi, "apply %d", argc);
@@ -1660,6 +1654,13 @@ boolector_apply (Btor *btor, int argc, BtorNode **args, BtorNode *fun)
 
   BTOR_TRAPI (strtrapi);
   BTOR_DELETEN (btor->mm, strtrapi, len);
+
+  i = btor_fun_sort_check (btor, argc, args, fun);
+  BTOR_ABORT_BOOLECTOR (i >= 0,
+                        "sort of argument at position %d does not match given"
+                        " function signature",
+                        i);
+
   btor->external_refs++;
   res = btor_apply_exps (btor, argc, args, fun);
   BTOR_TRAPI_RETURNP (res);
