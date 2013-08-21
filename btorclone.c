56c5e30fbda8513a9c6494730f4affdce91086e6
Aina Niemetz <aina.niemetz@gmail.com>
2013-08-21 11:17:59 +0200

    clone nodes_id_table first
    rather than traversing all reachable nodes via
    (un)synthesized_constraints, embedded_constraints, assumptions

--- btorclone.c
+++ btorclone.c
@@ -41,7 +41,7 @@ clone_aig (Btor *btor,
     res->children[i] = tmp;
   }
 
-  BTOR_PUSH_STACK (mm, *aigs, &real_aig->next);
+  BTOR_PUSH_STACK_IF (real_aig->next, mm, *aigs, &real_aig->next);
 
   return BTOR_IS_INVERTED_AIG (aig) ? BTOR_INVERT_AIG (res) : res;
 }
@@ -169,10 +169,11 @@ clone_exp (Btor *btor,
     if (real_exp->arity)
     {
       for (i = 0; i < real_exp->arity; i++)
-      {
-        res->e[i] = btor_mapped_node (exp_map, real_exp->e[i]);
-        assert (res->e[i]);
-      }
+        BTOR_PUSH_STACK (mm, *nodes, &real_exp->e[i]);
+      // {
+      //   res->e[i] = btor_mapped_node (exp_map, real_exp->e[i]);
+      //   assert (res->e[i]);
+      // }
     }
     else
     {
@@ -229,119 +230,186 @@ clone_exp (Btor *btor,
 }
 
 static void
-clone_constraints (Btor *btor,
-                   Btor *clone,
-                   BtorNodeMap *exp_map,
-                   BtorAIGMap *aig_map)
+clone_nodes_id_table (Btor *btor,
+                      Btor *clone,
+                      BtorNodeMap *exp_map,
+                      BtorAIGMap *aig_map)
 {
   assert (btor);
   assert (clone);
   assert (exp_map);
   assert (aig_map);
 
-  int i;
-  BtorNode *cur, *real_cur, *cloned_exp, **parentp, *parent;
-  BtorNodePtrStack stack, unmark_stack;
-  BtorNodePtrPtrStack nodes;
+  int i, n;
   BtorAIG **next, *cloned_aig;
+  BtorNode **tmp, *cloned_exp;
+  BtorNodePtrPtrStack nodes;
   BtorAIGPtrPtrStack aigs;
-  BtorPtrHashBucket *b;
-  BtorPtrHashTable **c, **r;
   BtorMemMgr *mm;
 
-  BtorPtrHashTable *constraints[]     = {btor->unsynthesized_constraints,
-                                     btor->synthesized_constraints,
-                                     btor->embedded_constraints,
-                                     btor->assumptions,
-                                     NULL},
-                   *res_constraints[] = {clone->unsynthesized_constraints,
-                                         clone->synthesized_constraints,
-                                         clone->embedded_constraints,
-                                         clone->assumptions,
-                                         NULL};
-  BTOR_INIT_STACK (stack);
-  BTOR_INIT_STACK (unmark_stack);
   BTOR_INIT_STACK (nodes);
   BTOR_INIT_STACK (aigs);
 
   mm = btor->mm;
 
-  for (c = constraints, r = res_constraints; *c; c++, r++)
+  n = BTOR_COUNT_STACK (btor->nodes_id_table);
+  /* first element (id = 0) is empty, second element (id = 1) is true_exp
+   * and already cloned */
+  for (i = 2; i < n; i++)
   {
-    for (b = (*c)->first; b; b = b->next)
-    {
-      cur = (BtorNode *) b->key;
-      BTOR_PUSH_STACK (mm, stack, cur);
-      while (!BTOR_EMPTY_STACK (stack))
-      {
-        cur      = BTOR_POP_STACK (stack);
-        real_cur = BTOR_REAL_ADDR_NODE (cur);
-
-        if (real_cur->clone_mark >= 2
-            || real_cur == BTOR_REAL_ADDR_NODE (btor->true_exp))
-          continue;
-
-        assert (!btor_mapped_node (exp_map, cur));
-
-        if (real_cur->clone_mark == 0)
-        {
-          real_cur->clone_mark = 1;
-          BTOR_PUSH_STACK (mm, stack, cur);
-          BTOR_PUSH_STACK (mm, unmark_stack, real_cur);
-
-          if (BTOR_IS_ARRAY_EQ_NODE (real_cur) && real_cur->vreads)
-          {
-            BTOR_PUSH_STACK (mm, stack, real_cur->vreads->exp1);
-            BTOR_PUSH_STACK (mm, stack, real_cur->vreads->exp2);
-          }
-
-          for (i = 0; i < real_cur->arity; i++)
-            BTOR_PUSH_STACK (mm, stack, real_cur->e[i]);
-        }
-        else
-        {
-          assert (real_cur->clone_mark == 1);
-          real_cur->clone_mark = 2;
-          clone_exp (btor, clone, cur, &nodes, &aigs, exp_map, aig_map);
-        }
-      }
-
-      cloned_exp = btor_mapped_node (exp_map, b->key);
-      assert (cloned_exp);
-      btor_insert_in_ptr_hash_table (*r, cloned_exp);
-    }
+    if (btor->nodes_id_table.start[i])
+      clone_exp (btor,
+                 clone,
+                 btor->nodes_id_table.start[i],
+                 &nodes,
+                 &aigs,
+                 exp_map,
+                 aig_map);
   }
 
-  /* update parent and next pointers of expressions */
+  /* update children, parent and next pointers of expressions */
   while (!BTOR_EMPTY_STACK (nodes))
   {
-    parentp = BTOR_POP_STACK (nodes);
-    parent  = *parentp;
-    assert (parent);
-    cloned_exp = btor_mapped_node (exp_map, parent);
-    /* if !cloned_exp, parent is not reachable via roots -> do not clone
-     * but skip implicitely via *parentp = cloned_exp = NULL */
-    *parentp = cloned_exp;
+    tmp = BTOR_POP_STACK (nodes);
+    assert (*tmp);
+    cloned_exp = btor_mapped_node (exp_map, *tmp);
+    assert (cloned_exp);
+    *tmp = cloned_exp;
   }
 
   /* update next pointers of aigs */
   while (!BTOR_EMPTY_STACK (aigs))
   {
     next = BTOR_POP_STACK (aigs);
-    if (!*next) continue;
+    assert (*next);
     cloned_aig = btor_mapped_aig (aig_map, *next);
     assert (cloned_aig);
     *next = cloned_aig;
   }
 
-  /* reset mark flags */
-  while (!BTOR_EMPTY_STACK (unmark_stack))
-  {
-    cur = BTOR_POP_STACK (unmark_stack);
-    assert (cur->clone_mark == 2);
-    cur->clone_mark = 0;
-  }
+  BTOR_RELEASE_STACK (mm, nodes);
+  BTOR_RELEASE_STACK (mm, aigs);
 }
+
+// static void
+// clone_constraints (Btor * btor, Btor * clone,
+//		   BtorNodeMap * exp_map, BtorAIGMap * aig_map)
+//{
+//  assert (btor);
+//  assert (clone);
+//  assert (exp_map);
+//  assert (aig_map);
+//
+//  int i;
+//  BtorNode *cur, *real_cur, *cloned_exp, **parentp, *parent;
+//  BtorNodePtrStack stack, unmark_stack;
+//  BtorNodePtrPtrStack nodes;
+//  BtorAIG **next, *cloned_aig;
+//  BtorAIGPtrPtrStack aigs;
+//  BtorPtrHashBucket *b;
+//  BtorPtrHashTable **c, **r;
+//  BtorMemMgr *mm;
+//
+//  BtorPtrHashTable *constraints[]     = { btor->unsynthesized_constraints,
+//				          btor->synthesized_constraints,
+//				          btor->embedded_constraints,
+//					  btor->assumptions,
+//				          NULL },
+//		   *res_constraints[] = { clone->unsynthesized_constraints,
+//					  clone->synthesized_constraints,
+//					  clone->embedded_constraints,
+//					  clone->assumptions,
+//					  NULL };
+//  BTOR_INIT_STACK (stack);
+//  BTOR_INIT_STACK (unmark_stack);
+//  BTOR_INIT_STACK (nodes);
+//  BTOR_INIT_STACK (aigs);
+//
+//  mm = btor->mm;
+//
+//  for (c = constraints, r = res_constraints; *c; c++, r++)
+//    {
+//      for (b = (*c)->first; b; b = b->next)
+//	{
+//	  cur = (BtorNode *) b->key;
+//	  BTOR_PUSH_STACK (mm, stack, cur);
+//	  while (!BTOR_EMPTY_STACK (stack))
+//	    {
+//	      cur = BTOR_POP_STACK (stack);
+//	      real_cur = BTOR_REAL_ADDR_NODE (cur);
+//
+//	      if (real_cur->clone_mark >= 2
+//		  || real_cur == BTOR_REAL_ADDR_NODE (btor->true_exp))
+//		continue;
+//
+//	      assert (!btor_mapped_node (exp_map, cur));
+//
+//	      if (real_cur->clone_mark == 0)
+//		{
+//		  real_cur->clone_mark = 1;
+//		  BTOR_PUSH_STACK (mm, stack, cur);
+//		  BTOR_PUSH_STACK (mm, unmark_stack, real_cur);
+//
+//		  if (BTOR_IS_ARRAY_EQ_NODE (real_cur) && real_cur->vreads)
+//		    {
+//		      BTOR_PUSH_STACK (mm, stack, real_cur->vreads->exp1);
+//		      BTOR_PUSH_STACK (mm, stack, real_cur->vreads->exp2);
+//		    }
+//
+//		  for (i = 0; i < real_cur->arity; i++)
+//		    BTOR_PUSH_STACK (mm, stack, real_cur->e[i]);
+//		}
+//	      else
+//		{
+//		  assert (real_cur->clone_mark == 1);
+//		  real_cur->clone_mark = 2;
+//		  clone_exp (btor, clone, cur, &nodes, &aigs,
+//			     exp_map, aig_map);
+//		}
+//	    }
+//
+//	  cloned_exp = btor_mapped_node (exp_map, b->key);
+//	  assert (cloned_exp);
+//	  btor_insert_in_ptr_hash_table (*r, cloned_exp);
+//	}
+//    }
+//
+//    /* update parent and next pointers of expressions */
+//    while (!BTOR_EMPTY_STACK (nodes))
+//      {
+//	parentp = BTOR_POP_STACK (nodes);
+//	parent = *parentp;
+//	assert (parent);
+//	cloned_exp = btor_mapped_node (exp_map, parent);
+//	/* if !cloned_exp, parent is not reachable via roots -> do not clone
+//	 * but skip implicitely via *parentp = cloned_exp = NULL */
+//	*parentp = cloned_exp;
+//      }
+//
+//    /* update next pointers of aigs */
+//    while (!BTOR_EMPTY_STACK (aigs))
+//      {
+//	next = BTOR_POP_STACK (aigs);
+//	if (!*next)
+//	  continue;
+//	cloned_aig = btor_mapped_aig (aig_map, *next);
+//	assert (cloned_aig);
+//	*next = cloned_aig;
+//      }
+//
+//    /* reset mark flags */
+//    while (!BTOR_EMPTY_STACK (unmark_stack))
+//      {
+//  	cur = BTOR_POP_STACK (unmark_stack);
+//  	assert (cur->clone_mark == 2);
+//  	cur->clone_mark = 0;
+//      }
+//
+//    BTOR_RELEASE_STACK (mm, stack);
+//    BTOR_RELEASE_STACK (mm, unmark_stack);
+//    BTOR_RELEASE_STACK (mm, nodes);
+//    BTOR_RELEASE_STACK (mm, aigs);
+//}
 
 static void
 clone_ptr_hash_table (BtorPtrHashTable *table,
@@ -407,10 +475,12 @@ btor_clone_btor (Btor *btor)
           &btor->stats,
           (char *) btor + sizeof (*btor) - (char *) &btor->stats);
 
-  clone_constraints (btor, clone, exp_map, aig_map);
+  // clone_constraints (btor, clone, exp_map, aig_map);
 
-  clone_node_ptr_stack (
-      clone->mm, &btor->nodes_id_table, &clone->nodes_id_table, exp_map);
+  // clone_node_ptr_stack (clone->mm, &btor->nodes_id_table,
+  //      		&clone->nodes_id_table, exp_map);
+
+  clone_nodes_id_table (btor, clone, exp_map, aig_map);
 
   *clone->nodes_unique_table.chains =
       btor_mapped_node (exp_map, *btor->nodes_unique_table.chains);
@@ -426,6 +496,14 @@ btor_clone_btor (Btor *btor)
   clone_ptr_hash_table (btor->lod_cache, clone->lod_cache, exp_map);
   clone_ptr_hash_table (
       btor->varsubst_constraints, clone->varsubst_constraints, exp_map);
+  clone_ptr_hash_table (
+      btor->embedded_constraints, clone->embedded_constraints, exp_map);
+  clone_ptr_hash_table (btor->unsynthesized_constraints,
+                        clone->unsynthesized_constraints,
+                        exp_map);
+  clone_ptr_hash_table (
+      btor->synthesized_constraints, clone->synthesized_constraints, exp_map);
+  clone_ptr_hash_table (btor->assumptions, clone->assumptions, exp_map);
   clone_ptr_hash_table (btor->var_rhs, clone->var_rhs, exp_map);
   clone_ptr_hash_table (btor->array_rhs, clone->array_rhs, exp_map);
 
