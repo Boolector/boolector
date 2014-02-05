1ba965d454dc3d1a2fa0bbb2796d54fc2a85a0c4
Mathias Preiner <mathias.preiner@gmail.com>
2014-02-05 09:35:34 +0100

    fix

--- boolector.c
+++ boolector.c
@@ -262,7 +262,6 @@ btor_chkclone_stats (Btor *btor)
   BTOR_CHKCLONE_STATS (beta_reduce_calls);
   BTOR_CHKCLONE_STATS (eval_exp_calls);
   BTOR_CHKCLONE_STATS (lambda_synth_apps);
-  BTOR_CHKCLONE_STATS (lambda_chains_merged);
   BTOR_CHKCLONE_STATS (lambdas_merged);
   BTOR_CHKCLONE_STATS (propagations);
   BTOR_CHKCLONE_STATS (propagations_down);
@@ -479,7 +478,7 @@ btor_chkclone_exp (BtorNode *exp, BtorNode *clone)
   BTOR_CHKCLONE_EXP (bytes);
   BTOR_CHKCLONE_EXP (parameterized);
   BTOR_CHKCLONE_EXP (lambda_below);
-  BTOR_CHKCLONE_EXP (chain);
+  BTOR_CHKCLONE_EXP (merge);
   BTOR_CHKCLONE_EXP (is_write);
   BTOR_CHKCLONE_EXP (is_read);
 
