62f43eb2eab0cb2a707c96912d28b13d8a1a03cf
Armin Biere <armin.biere@gmail.com>
2014-03-03 17:08:41 +0100

    fixed C year

--- btorexp.c
+++ btorexp.c
@@ -1,7 +1,7 @@
 /*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
  *
  *  Copyright (C) 2007-2009 Robert Daniel Brummayer.
- *  Copyright (C) 2007-2013 Armin Biere.
+ *  Copyright (C) 2007-2014 Armin Biere.
  *  Copyright (C) 2012-2013 Aina Niemetz.
  *  Copyright (C) 2012-2014 Mathias Preiner.
  *
