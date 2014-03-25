37a54d381b564cfa36f0486bf3c8ba869f63e79c
Armin Biere <armin.biere@gmail.com>
2014-03-25 22:28:31 +0100

    working testibv.cc

--- ibv.mk
+++ ibv.mk
@@ -1,7 +1,9 @@
 LIBOBJ+=btoribv.o
-all: btorimc
+all: btorimc testibv
 btorimc: btorimc.o libboolector.a $(LDEPS)
 	$(CXX) $(CFLAGS) -o $@ btorimc.o -L. -lboolector $(LIBS)
+testibv: testibv.o libboolector.a $(LDEPS)
+	$(CXX) $(CFLAGS) -o $@ testibv.o -L. -lboolector $(LIBS)
 clean: ibv.clean
 ibv.clean:
 	rm -f btorimc
