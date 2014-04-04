636c65919464fe5583757554b18f11727d53245c
Armin Biere <armin.biere@gmail.com>
2014-04-04 14:00:30 +0200

    swapped

--- btorexp.c
+++ btorexp.c
@@ -1753,7 +1753,25 @@ btor_eq_exp_node (Btor *btor, BtorNode *e0, BtorNode *e1)
   if (BTOR_IS_FUN_NODE (BTOR_REAL_ADDR_NODE (e0)))
     kind = BTOR_AEQ_NODE;
   else
+  {
     kind = BTOR_BEQ_NODE;
+    if (BTOR_IS_INVERTED_NODE (e0)
+        && BTOR_REAL_ADDR_NODE (e0)->kind == BTOR_BV_VAR_NODE)
+    {
+    SWAP:
+      e0 = BTOR_INVERT_NODE (e0);
+      e1 = BTOR_INVERT_NODE (e1);
+    }
+    else if (BTOR_IS_INVERTED_NODE (e1)
+             && BTOR_REAL_ADDR_NODE (e1)->kind == BTOR_BV_VAR_NODE
+             && (BTOR_IS_INVERTED_NODE (e0)
+                 || BTOR_REAL_ADDR_NODE (e1)->kind != BTOR_BV_VAR_NODE))
+    {
+      BtorNode *tmp = BTOR_INVERT_NODE (e0);
+      e0            = BTOR_INVERT_NODE (e1);
+      e1            = tmp;
+    }
+  }
 
   return binary_exp (btor, kind, e0, e1, 1);
 }
