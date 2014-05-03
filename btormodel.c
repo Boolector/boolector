06539e724e02e51700a4949379288de140858a21
Mathias Preiner <mathias.preiner@gmail.com>
2014-05-03 16:54:15 +0200

    btormodel: always compute assignment bottom up (from bv vars)
    we cannot use assignments of expression, whose tseitin flag is set since
    there is no guarantee that the current assignment is consistent (in case
    of partial models).

--- btormodel.c
+++ btormodel.c
@@ -259,19 +259,9 @@ recursively_compute_assignment (Btor *btor,
 
     if (real_cur->eval_mark == 0)
     {
-      if (real_cur->tseitin
-          && !BTOR_IS_FUN_NODE (real_cur)
-          /* always compute values for applies
-           * (must be consistent anyway) */
-          && !BTOR_IS_APPLY_NODE (real_cur))
-      {
-        assert (BTOR_IS_SYNTH_NODE (real_cur));
-        result = btor_assignment_bv (btor, real_cur, 0);
-        goto CACHE_AND_PUSH_RESULT;
-      }
       /* add assignment of bv var to model (creates new assignment, if
        * it doesn't have one) */
-      else if (BTOR_IS_BV_VAR_NODE (real_cur))
+      if (BTOR_IS_BV_VAR_NODE (real_cur))
       {
         result = btor_assignment_bv (btor, real_cur, 1);
         add_to_bv_model (btor, real_cur, result);
