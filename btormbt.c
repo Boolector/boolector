643388632a30f6a604503535c26e7f093fbbf69e
Aina Niemetz <aina.niemetz@gmail.com>
2014-09-16 12:50:05 +0200

    btormbt: disable sat solver switching if shadow clone is enabled

--- btormbt.c
+++ btormbt.c
@@ -2142,21 +2142,21 @@ _opt (BtorMBT *btormbt, unsigned r)
 
     /* set random sat solver */
 #ifdef BTOR_USE_LINGELING
-  if (pick (&rng, 0, 1) && set_sat_solver)
+  if (!btormbt->shadow && pick (&rng, 0, 1) && set_sat_solver)
   {
     boolector_set_sat_solver_lingeling (btormbt->btor, 0, 0);
     set_sat_solver = 0;
   }
 #endif
 #ifdef BTOR_USE_PICOSAT
-  if (pick (&rng, 0, 1) && set_sat_solver)
+  if (!btormbt->shadow && pick (&rng, 0, 1) && set_sat_solver)
   {
     boolector_set_sat_solver_picosat (btormbt->btor);
     set_sat_solver = 0;
   }
 #endif
 #ifdef BTOR_USE_MINISAT
-  if (pick (&rng, 0, 1) && set_sat_solver)
+  if (!btormbt->shadow && pick (&rng, 0, 1) && set_sat_solver)
     boolector_set_sat_solver_minisat (btormbt->btor);
 #endif
 
