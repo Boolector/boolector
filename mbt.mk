68e49ac140ff2b4822d5d4f129002152a6a55335
Mathias Preiner <mathias.preiner@gmail.com>
2014-09-30 09:43:36 +0200

    fix shared library handling

--- mbt.mk
+++ mbt.mk
@@ -6,6 +6,6 @@ btoruntrace: btoruntrace.c libboolector.a $(LDEPS)
 btormbt: btormbt.c libboolector.a $(LDEPS)
 	$(CC) $(CFLAGS) -o $@ btormbt.o -L. -lboolector $(LIBS)
 
-clean-mbt:
+clean: btormbt-clean
+btormbt-clean:
 	rm -f btoruntrace btormbt
-	rm -f btoruntrace.o btormbt.o
