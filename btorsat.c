5b800dad081c82488fea0c7549f3db7bed16c229
Mathias Preiner <mathias.preiner@gmail.com>
2014-10-19 19:14:28 +0200

    btorsat: print conflict limit

--- btorsat.c
+++ btorsat.c
@@ -2,7 +2,7 @@
  *
  *  Copyright (C) 2007-2009 Robert Daniel Brummayer.
  *  Copyright (C) 2007-2013 Armin Biere.
- *  Copyright (C) 2012 Mathias Preiner.
+ *  Copyright (C) 2012-2014 Mathias Preiner.
  *  Copyright (C) 2013-2014 Aina Niemetz.
  *
  *  All rights reserved.
@@ -242,7 +242,8 @@ btor_sat_sat (BtorSATMgr *smgr, int limit)
   int res;
   assert (smgr != NULL);
   assert (smgr->initialized);
-  BTOR_MSG (smgr->msg, 2, "calling SAT solver %s", smgr->name);
+  BTOR_MSG (
+      smgr->msg, 2, "calling SAT solver %s with limit %d", smgr->name, limit);
   assert (!smgr->satcalls || smgr->inc_required);
   smgr->satcalls++;
   res = smgr->api.sat (smgr, limit);
