76679593b498e87bb1bf8d53f693d32a451869af
Mathias Preiner <mathias.preiner@gmail.com>
2015-03-11 16:04:52 +0100

    added integer hash table with hopscotch hashing

---
+++ tests/testinthash.h
@@ -0,0 +1,20 @@
+/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2015 Mathias Preiner.
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of Boolector.
+ *  See COPYING for more information on using this software.
+ */
+
+#ifndef TESTINTHASH_H_INCLUDED
+#define TESTINTHASH_H_INCLUDED
+
+void init_int_hash_table_tests (void);
+
+void run_int_hash_table_tests (int argc, char **argv);
+
+void finish_int_hash_table_tests (void);
+
+#endif
