795237a9bdcbb0f31eb0153b068064920ce819d6
Mathias Preiner <mathias.preiner@gmail.com>
2015-04-18 18:28:48 +0200

    correctly handle varsubst constraints in check_model

--- btorcore.c
+++ btorcore.c
@@ -8059,30 +8059,6 @@ map_inputs_check_model (Btor *btor, Btor *clone)
 }
 
 static void
-reset_varsubst_constraints (Btor *btor)
-{
-  assert (btor);
-
-  BtorNode *right, *left;
-  BtorHashTableIterator it;
-
-  init_node_hash_table_iterator (&it, btor->varsubst_constraints);
-  while (has_next_node_hash_table_iterator (&it))
-  {
-    right = (BtorNode *) it.bucket->data.asPtr;
-    assert (right);
-    left = next_node_hash_table_iterator (&it);
-    btor_release_exp (btor, left);
-    btor_release_exp (btor, right);
-  }
-  btor_delete_ptr_hash_table (btor->varsubst_constraints);
-  btor->varsubst_constraints =
-      btor_new_ptr_hash_table (btor->mm,
-                               (BtorHashPtr) btor_hash_exp_by_id,
-                               (BtorCmpPtr) btor_compare_exp_by_id);
-}
-
-static void
 rebuild_formula (Btor *btor, int rewrite_level)
 {
   assert (btor);
@@ -8127,10 +8103,12 @@ check_model (Btor *btor, Btor *clone, BtorPtrHashTable *inputs)
 
   int ret;
   char *a;
-  BtorNode *cur, *exp, *simp, *real_simp, *subst;
+  BtorNode *cur, *exp, *simp, *real_simp, *model, *eq;
   BtorHashTableIterator it;
   const BtorPtrHashTable *fmodel;
 
+  /* formula did not change since last sat call, we have to reset assumptions
+   * from the previous run */
   if (clone->valid_assignments) reset_incremental_usage (clone);
 
   /* add assumptions as assertions */
@@ -8139,12 +8117,14 @@ check_model (Btor *btor, Btor *clone, BtorPtrHashTable *inputs)
     btor_assert_exp (clone, next_node_hash_table_iterator (&it));
   reset_assumptions (clone);
 
+  /* apply variable substitution until fixpoint */
+  while (clone->varsubst_constraints->count > 0) substitute_var_exps (clone);
+
   /* rebuild formula with new rewriting level */
   rebuild_formula (clone, 3);
 
   assert (!clone->substitutions);
   init_substitutions (clone);
-
   init_node_hash_table_iterator (&it, inputs);
   while (has_next_node_hash_table_iterator (&it))
   {
@@ -8155,41 +8135,42 @@ check_model (Btor *btor, Btor *clone, BtorPtrHashTable *inputs)
     cur = next_node_hash_table_iterator (&it);
     assert (BTOR_IS_REGULAR_NODE (cur));
     assert (cur->btor == clone);
-    // TODO (ma) why pointer_chase instead of btor_simplify_exp?
-    simp      = btor_pointer_chase_simplified_exp (clone, cur);
+    simp      = btor_simplify_exp (clone, cur);
     real_simp = BTOR_REAL_ADDR_NODE (simp);
 
-    if (BTOR_IS_BV_CONST_NODE (real_simp)
-        || btor_find_in_ptr_hash_table (clone->substitutions, real_simp))
-      continue;
+    if (btor_find_in_ptr_hash_table (clone->substitutions, real_simp)) continue;
 
-    if (BTOR_IS_BV_VAR_NODE (real_simp))
+    if (BTOR_IS_FUN_NODE (real_simp))
     {
-      assert (!BTOR_IS_FUN_NODE (real_simp));
-      /* we need to invert the assignment if simplified is inverted */
-      a     = (char *) btor_get_bv_model_str (btor,
-                                          BTOR_COND_INVERT_NODE (simp, exp));
-      subst = btor_const_exp (clone, a);
-      btor_release_bv_assignment_str (btor, a);
-    }
-    else
-    {
-      assert (BTOR_IS_FUN_NODE (real_simp));
       fmodel = btor_get_fun_model (btor, exp);
 
       if (!fmodel) continue;
 
-      subst = btor_generate_lambda_model_from_fun_model (clone, cur, fmodel);
+      model =
+          btor_generate_lambda_model_from_fun_model (clone, real_simp, fmodel);
+      assert (!btor_find_in_ptr_hash_table (clone->substitutions, real_simp));
+      // TODO (ma): as soon as we support extensionality we add an
+      //            equality of two functions
+      //            right now we substitute the original function with the
+      //            model, which still detects invalid models
+      insert_substitution (clone, real_simp, model, 0);
     }
-    assert (!btor_find_in_ptr_hash_table (clone->substitutions, real_simp));
-    insert_substitution (clone, real_simp, subst, 0);
-    btor_release_exp (clone, subst);
+    else
+    {
+      /* we need to invert the assignment if simplified is inverted */
+      a     = (char *) btor_get_bv_model_str (btor,
+                                          BTOR_COND_INVERT_NODE (simp, exp));
+      model = btor_const_exp (clone, a);
+      btor_release_bv_assignment_str (btor, a);
+      eq = btor_eq_exp (clone, real_simp, model);
+      btor_assert_exp (clone, eq);
+      btor_release_exp (clone, eq);
+    }
+    btor_release_exp (clone, model);
   }
 
-  reset_functions_with_model (clone);
   substitute_and_rebuild (clone, clone->substitutions, 0);
   delete_substitutions (clone);
-  reset_varsubst_constraints (clone); /* varsubst not required */
 
   clone->options.beta_reduce_all.val = 1;
   ret                                = btor_simplify (clone);
