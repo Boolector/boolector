ddf526b1b5e23c28e687d7b7568b544d141cb432
Mathias Preiner <mathias.preiner@gmail.com>
2015-07-06 16:03:25 +0200

    remove synthebtor from repository

--- synthebtor.mk
+++
@@ -1,8 +0,0 @@
-all: synthebtor
-
-synthebtor: synthebtor.o libboolector.a $(LDEPS)
-	$(CC) $(CFLAGS) $(INCS) -o $@ synthebtor.o -L. -lboolector $(LIBS)
-
-clean: synthebtor-clean
-synthebtor-clean:
-	rm -f synthebtor
