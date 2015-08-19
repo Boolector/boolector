ccd8786e6fa866409a82c12f8688185c8522d318
Armin Biere <armin.biere@gmail.com>
2015-08-19 09:53:45 +0200

    moved and added

---
+++ btorfmt/btorcat.c
@@ -0,0 +1,94 @@
+/*
+The BtorFMT software provides a generic parser for the BTOR format.
+In contrast to Boolector it falls under the following MIT style license:
+
+Copyright (c) 2012-2015, Armin Biere, Johannes Kepler University, Linz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include "btorfmt.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+static int close_input;
+static FILE* input_file;
+static const char* input_name;
+
+int
+main (int argc, char** argv)
+{
+  BtorFormatReader* reader;
+  BtorFormatLine** lines;
+  const char* err;
+  int i;
+  for (i = 1; i < argc; i++)
+  {
+    if (!strcmp (argv[i], "-h"))
+    {
+      fprintf (stderr, "usage: btorcat [-h] [ <btorfile> ]\n");
+      exit (1);
+    }
+    else if (argv[i][0] == '-')
+    {
+      fprintf (
+          stderr, "*** btorcat: invalid option '%s' (try '-h')\n", argv[i]);
+      exit (1);
+    }
+    else if (input_name)
+    {
+      fprintf (stderr, "*** btorcat: too many inputs (try '-h')\n");
+      exit (1);
+    }
+    else
+      input_name = argv[i];
+  }
+  if (!input_name)
+  {
+    input_file = stdin;
+    assert (!close_input);
+    input_name = "<stdin>";
+  }
+  else
+  {
+    input_file = fopen (input_name, "r");
+    if (!input_file)
+    {
+      fprintf (
+          stderr, "*** btorcat: can not open '%s' for reading\n", input_name);
+      exit (1);
+    }
+    close_input = 1;
+  }
+  reader = new_btor_format_reader ();
+  lines  = read_btor_format_lines (reader);
+  if (!lines)
+  {
+    errmsg = error_btor_format_reader (reader);
+    assert (errmsg);
+    fprintf (
+        stderr, "*** btorcat parse error in '%s' %s\n", input_name, errmsg);
+    exit (1);
+  }
+  delete_btor_format_reader (reader);
+  if (close_input) fclose (input_file);
+  return 0;
+}
--- btorfmt/btorfmt.c
+++ btorfmt/btorfmt.c
@@ -1,25 +1,41 @@
-/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
- *
- *  Copyright (C) 2012-2013 Armin Biere.
- *
- *  All rights reserved.
- *
- *  This file is part of Boolector.
- *  See COPYING for more information on using this software.
- */
+/*
+The BtorFMT software provides a generic parser for the BTOR format.
+In contrast to Boolector it falls under the following MIT style license:
+
+Copyright (c) 2012-2015, Armin Biere, Johannes Kepler University, Linz
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include "btorfmt.h"
 
 #include <assert.h>
 #include <ctype.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 
 struct BtorFormatReader
 {
-  char* error;
+  char *error, *prefix;
   BtorFormatLine** lines;
-  int szlines, nlines, szbuf, nbuf;
+  int verbosity, szlines, nlines, szbuf, nbuf;
   char* buf;
   struct
   {
@@ -34,7 +50,35 @@ new_btor_format_reader ()
   BtorFormatReader* res = malloc (sizeof *res);
   if (!res) return 0;
   memset (res, 0, sizeof *res);
+  res->prefix = strdup ("");
   return res;
+}
+
+void
+set_btor_format_reader_verbosity (BtorFormatReader* bfr, int verbosity)
+{
+  bfr->verbosity = verbosity;
+}
+
+void
+set_btor_format_reader_prefix (BtorFormatReader* bfr, char* prefix)
+{
+  free (bfr->prefix);
+  bfr->prefix = strdup (prefix ? prefix : "");
+}
+
+static void
+msg_bfr (BtorFormatReader* bfr, int level, const char* fmt, ...)
+{
+  va_list ap;
+  if (bfr->verbosity < level) return;
+  ap = va_start (ap, fmt);
+  fflush (stdout);
+  fputs (bfs->prefix, stderr);
+  vfprintf (stderr, fmt, ap);
+  va_end (ap);
+  fputc ('\n', stderr);
+  fflush (stdrr);
 }
 
 static void
@@ -56,6 +100,7 @@ reset_bfr (BtorFormatReader* bfr)
     bfr->lines  = 0;
     bfr->nlines = bfr->szlines = 0;
   }
+  free (bfr->prefix);
 }
 
 void
