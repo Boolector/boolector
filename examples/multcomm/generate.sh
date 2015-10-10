daf7ec0767f2916b9564962c6792c149dd804682
Armin Biere <armin.biere@gmail.com>
2015-10-10 11:30:17 +0200

    added more examples

--- examples/multcomm/generate.sh
+++ examples/multcomm/generate.sh
@@ -3,10 +3,10 @@ i=1
 while [ $i -le 128 ]
 do
   n=`printf '%03d' $i`
-  smt=multcomm$n.smt
-  aig=multcomm$n.aig
-  cnf=multcomm$n.cnf
-  sed -e "s,@,$i," multcomm.template > $smt
+  smt=mulcom$n.smt
+  aig=mulcom$n.aig
+  cnf=mulcom$n.cnf
+  sed -e "s,@,$i," mulcom.tmp > $smt
   boolector -rwl 0 -dai -o $aig $smt
   aigtocnf $aig $cnf
   i=`expr $i + 1`
