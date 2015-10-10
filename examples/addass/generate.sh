7d4a48d0ae1a6188ceb7315ac01d1b4c4196fee1
Armin Biere <armin.biere@gmail.com>
2015-10-10 16:04:55 +0200

    fixed

--- examples/addass/generate.sh
+++ examples/addass/generate.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 i=1
-while [ $i -le 16 ]
+while [ $i -le 8192 ]
 do
-  n=`printf '%03d' $i`
+  n=`printf '%04d' $i`
   smt=addass$n.smt
   aig=addass$n.aig
   cnf=addass$n.cnf
   sed -e "s,@,$i," addass.tmp > $smt
   boolector -rwl 2 -dai -o $aig $smt
   aigtocnf $aig $cnf
-  i=`expr $i + 1`
+  i=`expr $i \* 2`
 done
