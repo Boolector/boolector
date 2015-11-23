4ba713b31e623f89ff8c8a5555a0c64e3159eb34
Aina Niemetz <aina.niemetz@gmail.com>
2015-11-23 13:56:51 +0100

    prop: amended inv_urem

--- btorprop.c
+++ btorprop.c
@@ -2831,7 +2831,7 @@ inv_urem_bv (Btor *btor,
   assert (eidx >= 0 && eidx <= 1);
   assert (!BTOR_IS_BV_CONST_NODE (BTOR_REAL_ADDR_NODE (urem->e[eidx])));
 
-  uint32_t bw;
+  uint32_t bw, cnt;
   int cmp;
   BtorNode *e;
   BtorBitVector *res, *bvmax, *tmp, *tmp2, *one, *n, *mul, *up, *sub;
@@ -2926,24 +2926,36 @@ inv_urem_bv (Btor *btor,
         {
           /* 1 <= n < (bve - bvurem) / bvurem (non-truncating)
            * (note: div truncates towards 0!) */
-          tmp  = btor_urem_bv (mm, sub, bvurem);
-          tmp2 = btor_udiv_bv (mm, sub, bvurem);
-          if (btor_is_zero_bv (tmp))
+
+          /* bvurem = 0 -> 1 <= n <= bve */
+          if (btor_is_zero_bv (bvurem))
           {
-            /* (bve - bvurem) / bvurem is not truncated (remainder
-             * is 0), therefore the EXclusive upper bound
-             * -> up = (bve - bvurem) / bvurem - 1 */
-            up = btor_sub_bv (mm, tmp2, one);
-            btor_free_bv (mm, tmp2);
+            up = btor_copy_bv (mm, bve);
           }
+          /* e[1] > bvurem
+           * -> (bve - bvurem) / n > bvurem
+           * -> (bve - bvurem) / bvurem > n */
           else
           {
-            /* (bve - bvurem) / bvurem  is truncated (remainder
-             * is not 0), therefore the INclusive upper bound
-             * -> up = (bve - bvurem) / bvurem */
-            up = tmp2;
+            tmp  = btor_urem_bv (mm, sub, bvurem);
+            tmp2 = btor_udiv_bv (mm, sub, bvurem);
+            if (btor_is_zero_bv (tmp))
+            {
+              /* (bve - bvurem) / bvurem is not truncated (remainder
+               * is 0), therefore the EXclusive upper bound
+               * -> up = (bve - bvurem) / bvurem - 1 */
+              up = btor_sub_bv (mm, tmp2, one);
+              btor_free_bv (mm, tmp2);
+            }
+            else
+            {
+              /* (bve - bvurem) / bvurem  is truncated (remainder
+               * is not 0), therefore the INclusive upper bound
+               * -> up = (bve - bvurem) / bvurem */
+              up = tmp2;
+            }
+            btor_free_bv (mm, tmp);
           }
-          btor_free_bv (mm, tmp);
 
           if (btor_is_zero_bv (up))
             res = btor_udiv_bv (mm, sub, one);
@@ -2953,15 +2965,20 @@ inv_urem_bv (Btor *btor,
              * s.t (bve - bvurem) % n = 0 */
             n   = btor_new_random_range_bv (mm, &btor->rng, bw, one, up);
             tmp = btor_urem_bv (mm, sub, n);
-            while (!btor_is_zero_bv (tmp))
+            for (cnt = 0; cnt < bw && !btor_is_zero_bv (tmp); cnt++)
             {
               btor_free_bv (mm, n);
               btor_free_bv (mm, tmp);
               n   = btor_new_random_range_bv (mm, &btor->rng, bw, one, up);
               tmp = btor_urem_bv (mm, sub, n);
             }
+
             /* res = (bve - bvurem) / n */
-            res = btor_udiv_bv (mm, sub, n);
+            if (btor_is_zero_bv (tmp)) res = btor_udiv_bv (mm, sub, n);
+            /* fallback: n = 1 */
+            else
+              res = btor_copy_bv (mm, sub);
+
             btor_free_bv (mm, n);
             btor_free_bv (mm, tmp);
           }
