fdba6f8a1799cf4fbe6a19b78a266ea10597b20f
Mathias Preiner <mathias.preiner@gmail.com>
2016-02-11 15:11:45 +0100

    added number partition generator for partitions of size 2 and 3

---
+++ utils/btorpartgen.c
@@ -0,0 +1,108 @@
+/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2016 Mathias Preiner.
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of Boolector.
+ *  See COPYING for more information on using this software.
+ */
+
+#include "utils/btorpartgen.h"
+
+#include <assert.h>
+#include <string.h>
+
+void
+btor_init_part_gen (BtorPartitionGenerator* pg,
+                    uint32_t n,
+                    uint32_t size,
+                    bool permutate)
+{
+  assert (size == 2 || size == 3);
+
+  uint32_t max;
+  pg->n         = n;
+  pg->cnt_1     = 1;
+  max           = n - size + 1;
+  pg->cnt_2     = (size == 2) ? max : 1;
+  pg->cnt_3     = (size == 3) ? max : 0;
+  pg->size      = size;
+  pg->permutate = permutate;
+  pg->perm_idx  = 0;
+  pg->perm_cnt  = 0;
+  memset (pg->tuple, 0, sizeof (uint32_t) * BTOR_PART_GEN_MAX_TUPLE_SIZE);
+}
+
+uint32_t*
+btor_next_part_gen (BtorPartitionGenerator* pg)
+{
+  assert (btor_has_next_part_gen (pg));
+
+  uint32_t tmp, perm_idx, swap_idx;
+
+  /* permuatate current tuple until all permuatations are done */
+  if (pg->permutate && pg->perm_cnt > 0)
+  {
+    do
+    {
+      perm_idx     = pg->perm_idx;
+      swap_idx     = (pg->perm_idx + 1) % pg->size;
+      pg->perm_idx = swap_idx;  //(pg->perm_idx + 1) % pg->size;
+      assert (perm_idx < pg->size);
+      assert (swap_idx < pg->size);
+    }
+    /* tuple does not change if values to be swapped are equal */
+    while (pg->tuple[perm_idx] == pg->tuple[swap_idx]);
+
+    tmp                 = pg->tuple[perm_idx];
+    pg->tuple[perm_idx] = pg->tuple[swap_idx];
+    pg->tuple[swap_idx] = tmp;
+    pg->perm_cnt--;
+    pg->perm_idx = swap_idx;
+    //      printf ("swap (%u) %u -> %u\n", pg->perm_cnt, tmp_idx, tmp_idx + 1);
+  }
+  else if (pg->size == 2)
+  {
+    pg->tuple[0] = pg->cnt_1;
+    pg->tuple[1] = pg->cnt_2;
+    pg->cnt_1++;
+    pg->cnt_2    = pg->n - pg->cnt_1;
+    pg->perm_idx = 0;
+    if (pg->tuple[0] == pg->tuple[1])
+      pg->perm_cnt = 0;
+    else
+      pg->perm_cnt = 1;
+  }
+  else
+  {
+    assert (pg->size == 3);
+    pg->tuple[0] = pg->cnt_1;
+    pg->tuple[1] = pg->cnt_2;
+    pg->tuple[2] = pg->cnt_3;
+    pg->cnt_3--;
+    pg->cnt_2 = pg->n - pg->cnt_1 - pg->cnt_3;
+    if (pg->cnt_2 > pg->cnt_3)
+    {
+      pg->cnt_1++;
+      pg->cnt_2 = pg->cnt_1;
+      pg->cnt_3 = pg->n - pg->cnt_1 - pg->cnt_2;
+    }
+    pg->perm_idx = 0;
+    if (pg->tuple[0] == pg->tuple[1] && pg->tuple[1] == pg->tuple[2])
+      pg->perm_cnt = 0;
+    else if (pg->tuple[0] == pg->tuple[1] || pg->tuple[0] == pg->tuple[2]
+             || pg->tuple[1] == pg->tuple[2])
+      pg->perm_cnt = 2;
+    else
+      pg->perm_cnt = 5;
+  }
+  return pg->tuple;
+}
+
+bool
+btor_has_next_part_gen (BtorPartitionGenerator* pg)
+{
+  if (pg->size == 2) return pg->cnt_1 <= pg->cnt_2;
+  return pg->cnt_1 <= pg->cnt_3 || (pg->permutate && pg->perm_cnt > 0);
+}
---
+++ utils/btorpartgen.h
@@ -0,0 +1,43 @@
+/*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2016 Mathias Preiner.
+ *
+ *  All rights reserved.
+ *
+ *  This file is part of Boolector.
+ *  See COPYING for more information on using this software.
+ */
+
+#ifndef BTORPARTGEN_H_INCLUDED
+#define BTORPARTGEN_H_INCLUDED
+
+#define BTOR_PART_GEN_MAX_TUPLE_SIZE 3
+
+#include <stdbool.h>
+#include <stdint.h>
+
+struct BtorPartitionGenerator
+{
+  uint32_t n;
+  uint32_t cnt_1;
+  uint32_t cnt_2;
+  uint32_t cnt_3;
+  uint32_t size;
+  uint32_t tuple[BTOR_PART_GEN_MAX_TUPLE_SIZE];
+  bool permutate;
+  uint32_t perm_idx;
+  uint32_t perm_cnt;
+};
+
+typedef struct BtorPartitionGenerator BtorPartitionGenerator;
+
+void btor_init_part_gen (BtorPartitionGenerator* pg,
+                         uint32_t n,
+                         uint32_t size,
+                         bool permutate);
+
+uint32_t* btor_next_part_gen (BtorPartitionGenerator* pg);
+
+bool btor_has_next_part_gen (BtorPartitionGenerator* pg);
+
+#endif
