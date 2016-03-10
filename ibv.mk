d197a13283b4bdc77173430a629daab519a2958b
Aina Niemetz <aina.niemetz@gmail.com>
2016-03-10 12:50:14 +0100

    moved dir 'api' to dir 'src'

--- ibv.mk
+++ ibv.mk
@@ -2,17 +2,17 @@ LIBOBJ+=$(BUILDIR)/btoribv.o
 
 all: $(BINDIR)/btorimc $(BINDIR)/testibv
 
-$(BINDIR)/btorimc: $(BUILDIR)/btorimc.o $(BUILDIR)/libboolector.a $(LDEPS)
+$(BINDIR)/btorimc: $(BUILDIR)/btorimc.o $(BUILDIR)/btoribv.o $(BUILDIR)/libboolector.a $(LDEPS)
 	@mkdir -p $(@D)
-	$(CXX) $(CFLAGS) $(INCS) -o $@ -L$(BUILDIR) -lboolector $(LIBS)
+	$(CXX) $(CFLAGS) $(INCS) -o $@ $(BUILDIR)/btorimc.o $(BUILDIR)/btoribv.o -L$(BUILDIR) -lboolector $(LIBS)
 
-$(BUILDIR)/$(TESTDIR)/testibv.o: $(SRCDIR)/$(TESTDIR)/testibv.cc
+$(BUILDIR)/$(TESTDIR)/testibv.o: $(SRCDIR)/$(TESTDIR)/testibv.cc $(BUILDIR)/btoribv.o
 	@mkdir -p $(@D)
-	$(CXX) $(CFLAGS) $(INCS) -c $< -o $@
+	$(CXX) $(CFLAGS) $(INCS) -c $< -o $@ $(BUILDIR)/btoribv.o
 
-$(BINDIR)/testibv: $(BUILDIR)/$(TESTDIR)/testibv.o $(BUILDIR)/libboolector.a $(LDEPS)
+$(BINDIR)/testibv: $(BUILDIR)/$(TESTDIR)/testibv.o $(BUILDIR)/btoribv.o $(BUILDIR)/libboolector.a $(LDEPS)
 	@mkdir -p $(@D)
-	$(CXX) $(CFLAGS) $(INCS) -o $@ -L$(BUILDIR) -lboolector $(LIBS)
+	$(CXX) $(CFLAGS) $(INCS) -o $@ $(BUILDIR)/$(TESTDIR)/testibv.o $(BUILDIR)/btoribv.o -L$(BUILDIR) -lboolector $(LIBS)
 
 clean: ibv.clean
 
--- test.mk
+++ test.mk
@@ -11,9 +11,9 @@ $(BUILDIR)/$(TESTDIR)/test.dep: $(BUILDIR)/btorconfig.h $(SRCS) $(TESTSRCS) make
 	$(CC) $(CFLAGS) $(INCS) -MM $(TESTSRCS) | \
 	sed -e 's,:,: makefile,' -e 's,^test,$(BUILDIR)/$(TESTDIR)/test,' >$@
 
-$(BINDIR)/test: $(TESTOBJS) $(BUILDIR)/libboolector.a  $(LDEPS) makefile
+$(BINDIR)/test: $(TESTOBJS) $(BUILDIR)/btormain.o $(BUILDIR)/libboolector.a  $(LDEPS) makefile
 	@mkdir -p $(@D)
-	$(CC) $(CFLAGS) -o $@ $(TESTOBJS) $(INCS) -L$(BUILDIR) -lboolector $(LIBS)
+	$(CC) $(CFLAGS) -o $@ $(TESTOBJS) $(BUILDIR)/btormain.o $(INCS) -L$(BUILDIR) -lboolector $(LIBS)
 
 clean: test-clean
 
