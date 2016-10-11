860ab60d3b6b5728bc0e6a5ed6e5a8620e4fb993
Aina Niemetz <aina.niemetz@gmail.com>
2016-10-11 16:12:09 +0200

    btornodemap: removed obsolete functions
    now obsolete functions are provided via boolectormap and not internally
    relevant, moved/ported corresponding regression tests to boolectormap
    (testboolectormap)

--- src/tests/testmap.c
+++ src/tests/testmap.c
@@ -67,84 +67,8 @@ test_mapnewdel ()
 }
 
 /*------------------------------------------------------------------------*/
-
 void
 test_map0 ()
-{
-  init_map_test ();
-
-  BtorNode *a = btor_var_exp (g_btor, 32, "a");
-  BtorNode *b = btor_var_exp (g_btor, 32, "b");
-  BtorNode *s = btor_sub_exp (g_btor, a, b);
-  BtorNode *o = btor_one_exp (g_btor, 32);
-  BtorNode *t = btor_unsigned_exp (g_btor, 2, 32);
-
-  BtorNodeMap *map = btor_new_node_map (g_btor);
-  BtorNode *d;
-  btor_map_node (map, a, t);
-  btor_map_node (map, b, o);
-  d = btor_non_recursive_substitute_node (g_btor, map, s);
-  assert (d == o);
-  btor_delete_node_map (map);
-  // btor_release_exp (g_btor, d); // No, map owns reference!!!!!!
-
-  btor_release_exp (g_btor, a);
-  btor_release_exp (g_btor, b);
-  btor_release_exp (g_btor, o);
-  btor_release_exp (g_btor, s);
-  btor_release_exp (g_btor, t);
-
-  finish_map_test ();
-}
-
-/*------------------------------------------------------------------------*/
-
-static BtorNode *
-test_map1_mapper (Btor *btor, void *state, BtorNode *node)
-{
-  char *symbol;
-  (void) state;
-  assert (btor == g_btor);
-  assert (BTOR_IS_REGULAR_NODE (node));
-  if (!btor_is_bv_var_node (node)) return 0;
-  symbol = btor_get_symbol_exp (btor, node);
-  assert (symbol);
-  return btor_int_exp (btor, atoi (symbol), 8);
-}
-
-void
-test_map1 ()
-{
-  init_map_test ();
-
-  BtorNode *a = btor_var_exp (g_btor, 8, "11");
-  BtorNode *b = btor_var_exp (g_btor, 8, "22");
-  BtorNode *c = btor_var_exp (g_btor, 8, "33");
-  BtorNode *s;
-
-  BtorNode *sum = btor_add_exp (g_btor, a, b);
-  s             = btor_add_exp (g_btor, sum, c);
-  btor_release_exp (g_btor, sum);
-
-  BtorNodeMap *map = btor_new_node_map (g_btor);
-  BtorNode *d, *g;
-  d = btor_non_recursive_extended_substitute_node (
-      g_btor, map, 0, test_map1_mapper, btor_release_exp, s);
-  g = btor_int_exp (g_btor, 66, 8);
-  assert (d == g);
-  btor_release_exp (g_btor, g);
-  btor_delete_node_map (map);
-
-  btor_release_exp (g_btor, a);
-  btor_release_exp (g_btor, b);
-  btor_release_exp (g_btor, c);
-  btor_release_exp (g_btor, s);
-
-  finish_map_test ();
-}
-
-void
-test_map2 ()
 {
   Btor *stor       = btor_new_btor ();
   Btor *dtor       = btor_new_btor ();
@@ -165,7 +89,7 @@ test_map2 ()
 }
 
 void
-test_map3 ()
+test_map1 ()
 {
   Btor *stor       = btor_new_btor ();
   Btor *dtor       = btor_new_btor ();
@@ -193,6 +117,4 @@ run_map_tests (int argc, char **argv)
   BTOR_RUN_TEST (mapnewdel);
   BTOR_RUN_TEST (map0);
   BTOR_RUN_TEST (map1);
-  BTOR_RUN_TEST (map2);
-  BTOR_RUN_TEST (map3);
 }
--- src/utils/btornodemap.h
+++ src/utils/btornodemap.h
@@ -12,18 +12,7 @@
 #ifndef BTORNODEMAP_H_INCLUDED
 #define BTORNODEMAP_H_INCLUDED
 
-/*========================================================================*/
-/* After 'defactorizing' this code into 'btornodemap.h' and 'boolectormap.h'
- * this internal version became obsolete and currently only is used in the
- * regression suite.  We might want to keep it around for a while until
- * the external version in 'boolectormap.h' which goes through the external
- * API (that is 'boolector_...' functions and not 'btor_...' function) is
- * stable.  This split into two versions became necessary after we introduced
- * and now check proper external reference counting.
- */
-/*========================================================================*/
-
-#include "boolector.h"
+#include "btortypes.h"
 #include "utils/btorhashptr.h"
 
 /*------------------------------------------------------------------------*/
@@ -50,36 +39,6 @@ BtorNode *btor_mapped_node (BtorNodeMap *map, const BtorNode *node);
 int btor_count_map (const BtorNodeMap *map);
 void btor_map_node (BtorNodeMap *map, BtorNode *src, BtorNode *dst);
 void btor_delete_node_map (BtorNodeMap *map);
-
-/*------------------------------------------------------------------------*/
-
-BtorNode *btor_non_recursive_substitute_node (Btor *btor,
-                                              BtorNodeMap *map,
-                                              BtorNode *root);
-
-/*------------------------------------------------------------------------*/
-/* Extended mapping.  A 'BtorNodeMapper' function should return a NEW
- * reference to the result of mapping the argument node (using the arbitrary
- * state) or a 0 pointer if it can not map it.  The idea is that such a
- * mapper implements the base case of a (non-recursive) substitution.
- *
- * The mapper will only be called with non-inverted and simplified
- * nodes as arguments.
- */
-typedef BtorNode *(*BtorNodeMapper) (Btor *btor, void *state, BtorNode *node);
-
-/* References returned by a 'BtorNodeMapper' are not restricted to be
- * allocated internally, hence we need a matching release operation.
- */
-typedef void (*BtorNodeReleaser) (Btor *btor, BtorNode *node);
-
-BtorNode *btor_non_recursive_extended_substitute_node (
-    Btor *btor,
-    BtorNodeMap *map,  // share/cache substitution results
-    void *state,       // for the mapper
-    BtorNodeMapper,    // see above
-    BtorNodeReleaser,  // see above
-    BtorNode *root);
 
 /*------------------------------------------------------------------------*/
 
