1f128671e0fabad998f8f8345ff3e890f7dc3a87
Mathias Preiner <mathias.preiner@gmail.com>
2016-11-22 15:07:46 +0100

    btorchkclone: exclude internal cleanup option from check

--- src/btorchkclone.c
+++ src/btorchkclone.c
@@ -359,7 +359,8 @@ chkclone_opts (Btor *btor)
     copt = &clone->options[o];
     assert (opt->internal == copt->internal);
     /* Note: auto_cleanup.val = 1 in clone! */
-    if (o != BTOR_OPT_AUTO_CLEANUP) assert (opt->val == copt->val);
+    if (o != BTOR_OPT_AUTO_CLEANUP && o != BTOR_OPT_AUTO_CLEANUP_INTERNAL)
+      assert (opt->val == copt->val);
     assert (opt->dflt == copt->dflt);
     assert (opt->min == copt->min);
     assert (opt->max == copt->max);
