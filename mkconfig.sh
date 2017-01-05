694dd8dc8b15f2eef3d633dae5e64d0bf9249c56
Mathias Preiner <mathias.preiner@gmail.com>
2017-01-05 13:23:52 +0100

    mkconfig: fix

--- mkconfig.sh
+++ mkconfig.sh
@@ -13,7 +13,7 @@ config=$1
 if [ -f $config ]; then
   commit_conf=`grep BTOR_ID $config | cut -d ' ' -f 3 | grep -o -e '[0-9a-z]\+'`
   commit_cur=`./getgitid.sh`
-  if [ "$commit_conf" == "$commit_cur" ]; then
+  if [ "$commit_conf" = "$commit_cur" ]; then
     echo "[mkconfig.sh] commit id not changed, do not update $config"
     exit
   fi
