e00632b8b1cf5e83bb6828661a8cbae355350cc2
Aina Niemetz <aina.niemetz@gmail.com>
2017-04-24 11:50:47 +0200

    copyright

--- src/btormc.h
+++ src/btormc.h
@@ -1,7 +1,7 @@
 /*  Boolector: Satisfiablity Modulo Theories (SMT) solver.
  *
  *  Copyright (C) 2012-2013 Armin Biere.
- *  Copyright (C) 2016 Aina Niemetz.
+ *  Copyright (C) 2016-2017 Aina Niemetz.
  *
  *  All rights reserved.
  *
