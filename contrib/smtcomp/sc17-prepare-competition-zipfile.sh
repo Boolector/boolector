52a2acdc2febcf62e577a10d011f0dbf88bdaff6
Mathias Preiner <mathias.preiner@gmail.com>
2017-06-19 10:25:10 +0200

    final competition script

--- contrib/smtcomp/sc17-prepare-competition-zipfile.sh
+++ contrib/smtcomp/sc17-prepare-competition-zipfile.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# main track QF_ABV, QF_AUFBV, QF_UFBV, QF_BV, BV (with lingeling)
 make clean
 
 ./configure.sh -static --only-lingeling -flto
@@ -9,7 +11,6 @@ version=boolector-${b}-${l}
 dir=/tmp/boolector-smtcomp
 rm -rf $dir
 
-# configuration QF_ABV, QF_UFBV, QF_AUFBV, BV, QF_BV
 archive=${version}-main.tar.gz
 mkdir $dir
 mkdir $dir/bin
@@ -22,22 +23,22 @@ tar -C $dir -zcf $archive .
 rm -rf $dir
 ls -l $archive
 
+# main track QF_BV (with CaDiCaL)
 make clean
-./configure.sh -static --only-cadical -flto
+./configure.sh -static --cadical --lingeling --no-minisat --no-picosat -flto
 make
 b=`./bin/boolector -v /dev/null|grep -i version|grep -i boolector|awk '{print $(NF-1);exit}'`
 version=boolector-${b}
 dir=/tmp/boolector-smtcomp
 rm -rf $dir
 
-# configuration QF_BV with CaDiCaL
 archive=${version}-cadical-main-qf_bv.tar.gz
 mkdir $dir
 mkdir $dir/bin
 cp bin/boolector $dir/bin
 echo "#!/bin/sh
 
-./boolector -uc --fun:preprop --prop:nprops=10000 \$1" > $dir/bin/starexec_run_boolector
+./boolector -uc --fun:preprop --prop:nprops=10000 -SE=cadical \$1" > $dir/bin/starexec_run_boolector
 
 tar -C $dir -zcf $archive .
 rm -rf $dir
