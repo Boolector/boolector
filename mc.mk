71d9a593b94dd4b250d2755e7a67da318e8fe3a0
Armin Biere <armin.biere@gmail.com>
2018-01-28 14:21:36 +0100

    building btorsim

--- mc.mk
+++ mc.mk
@@ -4,9 +4,13 @@ $(BINDIR)/btormc: $(BUILDIR)/btormcmain.o $(LDEPS)
 	@mkdir -p $(@D)
 	$(CC) $(CFLAGS) $(INCS) -o $@ $(BUILDIR)/btormcmain.o -L$(BUILDIR) -lboolector $(LIBS)
 
+$(BINDIR)/btorsim: $(BUILDIR)/btorsimmain.o $(LDEPS)
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) $(INCS) -o $@ $(BUILDIR)/btorsimmain.o -L$(BUILDIR) -lboolector $(LIBS)
 
 clean: btormc.clean
 
 btormc.clean:
 	rm -f $(BUILDIR)/btormc $(BUILDIR)/btormc.o $(BUILDIR)/btormcmain.o
+	rm -f $(BUILDIR)/btorsim $(BUILDIR)/btorsimmain.o
 
