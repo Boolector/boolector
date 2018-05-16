2961996e39678ab35c0a48284693745c2bc37bff
Aina Niemetz <aina.niemetz@gmail.com>
2018-05-15 20:20:37 -0700

    btormc examples: Clean up run script

--- examples/btormc/run-examples.sh
+++ examples/btormc/run-examples.sh
@@ -1,64 +1,94 @@
 #!/bin/sh
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly RED='\033[0;31m'
+readonly NOCOLOR='\033[0m'
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly OUTDIR=$DIR/out
+readonly MCWITNESSDIR=$DIR/mc-witnesses
 
-btormc=$dir/../../bin/btormc
-btorsim=$dir/../../../btor2tools/bin/btorsim
+BTORMC=$DIR/../../bin/btormc
+if [ ! -e $BTORMC ]
+then
+  echo "[error] BtorMC not built"
+  exit 1
+fi
+BTORSIM=$DIR/../../../btor2tools/bin/btorsim
+if [ ! -e $BTORSIM ]
+then
+  echo "[error] BtorSim not built"
+  exit 1
+fi
 
 invalid () {
-  echo invalid
+  echo "$RED invalid$NOCOLOR"
 }
 
 set -x
 
- $btormc    -kmax 20  count2.btor2                  > count2.witnessmc
- $btormc    -kmax 20  count4.btor2                  > count4.witnessmc
- $btormc    -kmax 20  factorial4even.btor2          > factorial4even.witnessmc
- $btormc    -kmax 20  noninitstate.btor2            > noninitstate.witnessmc
- $btormc    -kmax 20  recount4.btor2                > recount4.witnessmc
- $btormc    -kmax 20  twocount2.btor2               > twocount2.witnessmc
- $btormc    -kmax 20  twocount2c.btor2              > twocount2c.witnessmc
- $btormc    -kmax 20  twocount32.btor2              > twocount32.witnessmc
-           
- $btorsim -b 0 -r 20  count2.btor2                  > count2.witnesssim
- $btorsim -b 0 -r 20  count4.btor2                  > count4.witnesssim
- $btorsim -b 0 -r 20  factorial4even.btor2          > factorial4even.witnesssim
- $btorsim -b 0 -r 20  noninitstate.btor2 -s 1       > noninitstate.witnesssim
- $btorsim -b 0 -r 20  twocount2.btor2               > twocount2.witnesssim
- $btorsim -b 0 -r 20  twocount2c.btor2   -s 11      > twocount2c.witnesssim
- $btorsim -b 0 -r 20  twocount32.btor2              > twocount32.witnesssim
+mkdir -p $OUTDIR
+mkdir -p $MCWITNESSDIR
 
-           
- $btorsim      -c     count2.btor2                    count2.witnessmc
- $btorsim      -c     count4.btor2                    count4.witnessmc
- $btorsim      -c     factorial4even.btor2            factorial4even.witnessmc
- $btorsim      -c     noninitstate.btor2              noninitstate.witnessmc
- $btorsim      -c     recount4.btor2                  recount4.witnessmc
- $btorsim      -c     twocount2.btor2                 twocount2.witnessmc
- $btorsim      -c     twocount2c.btor2                twocount2c.witnessmc
- $btorsim      -c     twocount32.btor2                twocount32.witnessmc
-           
- $btorsim      -c     count2.btor2                    count2.witnesssim
- $btorsim      -c     count4.btor2                    count4.witnesssim
- $btorsim      -c     factorial4even.btor2            factorial4even.witnesssim
- $btorsim      -c     noninitstate.btor2              noninitstate.witnesssim
- $btorsim      -c     twocount2.btor2                 twocount2.witnesssim
- $btorsim      -c     twocount2c.btor2                twocount2c.witnesssim
- $btorsim      -c     twocount32.btor2                twocount32.witnesssim
+### Run model checker
+$BTORMC    -kmax 20  count2.btor2             > $MCWITNESSDIR/count2.witnessmc
+$BTORMC    -kmax 20  count4.btor2             > $MCWITNESSDIR/count4.witnessmc
+$BTORMC    -kmax 20  factorial4even.btor2     > $MCWITNESSDIR/factorial4even.witnessmc
+$BTORMC    -kmax 20  noninitstate.btor2       > $MCWITNESSDIR/noninitstate.witnessmc
+$BTORMC    -kmax 20  recount4.btor2           > $MCWITNESSDIR/recount4.witnessmc
+$BTORMC    -kmax 20  twocount2.btor2          > $MCWITNESSDIR/twocount2.witnessmc
+$BTORMC    -kmax 20  twocount2c.btor2         > $MCWITNESSDIR/twocount2c.witnessmc
+$BTORMC    -kmax 20  twocount32.btor2         > $MCWITNESSDIR/twocount32.witnessmc
 
- $btorsim -b 0 -r 20  noninitstate.btor2            > noninitstate.nowitnesssim
- $btorsim      -c     noninitstate.btor2              noninitstate.nowitnesssim 2>/dev/null && invalid
-
- $btorsim -b 0 -r 20  recount4.btor2                > recount4.nowitnesssim
- $btorsim      -c     recount4.btor2                  recount4.witnesssim       2>/dev/null && invalid
-
- $btorsim -b 0 -r 20  twocount2c.btor2              > twocount2c.witnesssim
- $btorsim      -c     twocount2c.btor2                twocount2c.witnesssim     2>/dev/null && invalid
+### Check witnesses from model checker
+$BTORSIM      -c     count2.btor2               $MCWITNESSDIR/count2.witnessmc
+$BTORSIM      -c     count4.btor2               $MCWITNESSDIR/count4.witnessmc
+$BTORSIM      -c     factorial4even.btor2       $MCWITNESSDIR/factorial4even.witnessmc
+$BTORSIM      -c     noninitstate.btor2         $MCWITNESSDIR/noninitstate.witnessmc
+$BTORSIM      -c     recount4.btor2             $MCWITNESSDIR/recount4.witnessmc
+$BTORSIM      -c     twocount2.btor2            $MCWITNESSDIR/twocount2.witnessmc
+$BTORSIM      -c     twocount2c.btor2           $MCWITNESSDIR/twocount2c.witnessmc
+$BTORSIM      -c     twocount32.btor2           $MCWITNESSDIR/twocount32.witnessmc
 
 
+### Simulation for sat files, simulator produces valid witnesses
+# Run simulation
+$BTORSIM -b 0 -r 20  count2.btor2             > $OUTDIR/count2.witnesssim
+$BTORSIM -b 0 -r 20  count4.btor2             > $OUTDIR/count4.witnesssim
+$BTORSIM -b 0 -r 20  factorial4even.btor2     > $OUTDIR/factorial4even.witnesssim
+$BTORSIM -b 0 -r 20  noninitstate.btor2 -s 1  > $OUTDIR/noninitstate.witnesssim
+$BTORSIM -b 0 -r 20  twocount2.btor2          > $OUTDIR/twocount2.witnesssim
+$BTORSIM -b 0 -r 20  twocount2c.btor2   -s 11 > $OUTDIR/twocount2c.witnesssim
+$BTORSIM -b 0 -r 20  twocount32.btor2         > $OUTDIR/twocount32.witnesssim
+# Check witnesses produced by simulation
+$BTORSIM      -c     count2.btor2               $OUTDIR/count2.witnesssim
+$BTORSIM      -c     count4.btor2               $OUTDIR/count4.witnesssim
+$BTORSIM      -c     factorial4even.btor2       $OUTDIR/factorial4even.witnesssim
+$BTORSIM      -c     noninitstate.btor2         $OUTDIR/noninitstate.witnesssim
+$BTORSIM      -c     twocount2.btor2            $OUTDIR/twocount2.witnesssim
+$BTORSIM      -c     twocount2c.btor2           $OUTDIR/twocount2c.witnesssim
+$BTORSIM      -c     twocount32.btor2           $OUTDIR/twocount32.witnesssim
 
- $btorsim -b 0 -r 999 ponylink-slaveTXlen-sat.btor2 > ponylink-slaveTXlen.nowitnesssim
- $btorsim      -c     ponylink-slaveTXlen-sat.btor2   ponylink-slaveTXlen.nowitnesssim 2>/dev/null && invalid
 
- $btormc    -kmax 229 ponylink-slaveTXlen-sat.btor2 > ponylink-slaveTXlen.witnessmc
- $btorsim      -c     ponylink-slaveTXlen-sat.btor2   ponylink-slaveTXlen.witnessmc
+### Simulation for sat files, simulator produces invalid witnesses
+# Run simulation
+$BTORSIM -b 0 -r 20  noninitstate.btor2       > $OUTDIR/noninitstate.nowitnesssim
+# Check witness produced by simulation
+$BTORSIM      -c     noninitstate.btor2         $OUTDIR/noninitstate.nowitnesssim 2>/dev/null && invalid
+# Run simulation
+$BTORSIM -b 0 -r 20  recount4.btor2           > $OUTDIR/recount4.nowitnesssim
+# Check witness produced by simulation
+$BTORSIM      -c     recount4.btor2             $OUTDIR/recount4.nowitnesssim     2>/dev/null && invalid
+# Run simulation
+$BTORSIM -b 0 -r 20  twocount2c.btor2         > $OUTDIR/twocount2c.nowitnesssim
+# Check witness produced by simulation
+$BTORSIM      -c     twocount2c.btor2           $OUTDIR/twocount2c.nowitnesssim   2>/dev/null && invalid
+
+
+### Real world example
+# Run simulation (invalid witness)
+$BTORSIM -b 0 -r 999 ponylink-slaveTXlen-sat.btor2 > $OUTDIR/ponylink-slaveTXlen.nowitnesssim
+# Check witness from simulation
+$BTORSIM      -c     ponylink-slaveTXlen-sat.btor2   $OUTDIR/ponylink-slaveTXlen.nowitnesssim 2>/dev/null && invalid
+# Run model checker
+$BTORMC    -kmax 229 ponylink-slaveTXlen-sat.btor2 > $MCWITNESSDIR/ponylink-slaveTXlen.witnessmc
+# Check witness from model checker
+$BTORSIM      -c     ponylink-slaveTXlen-sat.btor2   $MCWITNESSDIR/ponylink-slaveTXlen.witnessmc
