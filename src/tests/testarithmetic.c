dbc3d184ea63d4bddf64871833d736f98add87ee
Aina Niemetz <aina.niemetz@gmail.com>
2018-06-13 14:43:33 -0700

    testarithmetic: Use API rather than boolector_main

--- src/tests/testarithmetic.c
+++ src/tests/testarithmetic.c
@@ -2,7 +2,7 @@
  *
  *  Copyright (C) 2007-2010 Robert Daniel Brummayer.
  *  Copyright (C) 2007-2012 Armin Biere.
- *  Copyright (C) 2012-2017 Aina Niemetz.
+ *  Copyright (C) 2012-2018 Aina Niemetz.
  *
  *  This file is part of Boolector.
  *  See COPYING for more information on using this software.
@@ -10,8 +10,7 @@
 
 #include "testarithmetic.h"
 
-#include "btorexit.h"
-#include "btormain.h"
+#include "boolector.h"
 #include "testrunner.h"
 #include "utils/btorutil.h"
 
@@ -25,62 +24,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BTOR_TEST_ARITHMETIC_TEMP_FILE_NAME "arith.tmp"
+#define BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME "arithin.tmp"
+#define BTOR_TEST_ARITHMETIC_TEMP_OUTFILE_NAME "arithout.tmp"
 
 #define BTOR_TEST_ARITHMETIC_LOW 1
 #define BTOR_TEST_ARITHMETIC_HIGH 4
 
-static int32_t g_argc   = 6;
-static char **g_argv    = NULL;
-static char *g_btor_str = NULL;
+static Btor *g_btor;
 
 void
 init_arithmetic_tests (void)
 {
-  FILE *f = fopen (BTOR_TEST_ARITHMETIC_TEMP_FILE_NAME, "w");
-  int32_t pos_rwr;
-
-  assert (f != NULL);
-  fclose (f);
-
-  pos_rwr = 0;
-
-  if (g_rwreads) pos_rwr = g_argc++ - 1;
-
-  g_btor_str = (char *) malloc (sizeof (char *) * (strlen (btor_bin_dir) + 20));
-  sprintf (g_btor_str, "%sboolector", btor_bin_dir);
-
-  g_argv = (char **) malloc (g_argc * sizeof (char *));
-
-  g_argv[0] = g_btor_str;
-  g_argv[1] = "-rwl";
-  g_argv[2] = "1";
-  g_argv[3] = "-o";
-  g_argv[4] = "/dev/null";
-
-  if (g_rwreads) g_argv[pos_rwr] = "-bra";
-
-  g_argv[g_argc - 1] = BTOR_TEST_ARITHMETIC_TEMP_FILE_NAME;
 }
 
 static void
 u_arithmetic_test (int32_t (*func) (int32_t, int32_t),
                    const char *func_name,
                    int32_t low,
-                   int32_t high)
+                   int32_t high,
+                   uint32_t rwl)
 {
-  FILE *f          = NULL;
+  assert (func != NULL);
+  assert (func_name != NULL);
+  assert (low > 0);
+  assert (low <= high);
+
+  FILE *fin        = NULL;
+  FILE *fout       = NULL;
   int32_t i        = 0;
   int32_t j        = 0;
   int32_t result   = 0;
   int32_t max      = 0;
   int32_t num_bits = 0;
   int32_t const_id = 0;
-  assert (func != NULL);
-  assert (func_name != NULL);
-  assert (low > 0);
-  assert (low <= high);
-  BtorExitCode exit_code = 0;
+  int32_t parse_res, parse_status;
+  char *parse_err;
+
   for (num_bits = low; num_bits <= high; num_bits++)
   {
     max = btor_util_pow_2 (num_bits);
@@ -91,27 +70,48 @@ u_arithmetic_test (int32_t (*func) (int32_t, int32_t),
         result = func (i, j);
         if (result < max)
         {
-          f = fopen (BTOR_TEST_ARITHMETIC_TEMP_FILE_NAME, "w");
-          assert (f != NULL);
-          fprintf (f, "1 constd %d %d\n", num_bits, i);
-          fprintf (f, "2 constd %d %d\n", num_bits, j);
-          fprintf (f, "3 %s %d 1 2\n", func_name, num_bits);
+          g_btor = boolector_new ();
+          boolector_set_opt (g_btor, BTOR_OPT_REWRITE_LEVEL, rwl);
+          if (g_rwreads)
+            boolector_set_opt (g_btor, BTOR_OPT_BETA_REDUCE_ALL, 1);
+
+          fin = fopen (BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME, "w");
+          assert (fin != NULL);
+          fprintf (fin, "1 constd %d %d\n", num_bits, i);
+          fprintf (fin, "2 constd %d %d\n", num_bits, j);
+          fprintf (fin, "3 %s %d 1 2\n", func_name, num_bits);
           if (result >= 0)
           {
-            fprintf (f, "4 constd %d %d\n", num_bits, result);
+            fprintf (fin, "4 constd %d %d\n", num_bits, result);
             const_id = 4;
           }
           else
           {
-            fprintf (f, "4 constd %d %d\n", num_bits, -result);
-            fprintf (f, "5 neg %d 4\n", num_bits);
+            fprintf (fin, "4 constd %d %d\n", num_bits, -result);
+            fprintf (fin, "5 neg %d 4\n", num_bits);
             const_id = 5;
           }
-          fprintf (f, "%d eq 1 3 %d\n", const_id + 1, const_id);
-          fprintf (f, "%d root 1 %d\n", const_id + 2, const_id + 1);
-          fclose (f);
-          exit_code = boolector_main (g_argc, g_argv);
-          assert (exit_code == BTOR_SAT_EXIT);
+          fprintf (fin, "%d eq 1 3 %d\n", const_id + 1, const_id);
+          fprintf (fin, "%d root 1 %d\n", const_id + 2, const_id + 1);
+          fclose (fin);
+          fin = fopen (BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME, "r");
+          assert (fin != NULL);
+          fout = fopen (BTOR_TEST_ARITHMETIC_TEMP_OUTFILE_NAME, "w");
+          assert (fout != NULL);
+          parse_res =
+              boolector_parse_btor (g_btor,
+                                    fin,
+                                    BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME,
+                                    fout,
+                                    &parse_err,
+                                    &parse_status);
+          assert (parse_res != BOOLECTOR_PARSE_ERROR);
+          assert (boolector_sat (g_btor) == BOOLECTOR_SAT);
+          fclose (fin);
+          fclose (fout);
+          boolector_delete (g_btor);
+          assert (remove (BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME) == 0);
+          assert (remove (BTOR_TEST_ARITHMETIC_TEMP_OUTFILE_NAME) == 0);
         }
       }
     }
@@ -122,9 +122,16 @@ static void
 s_arithmetic_test (int32_t (*func) (int32_t, int32_t),
                    const char *func_name,
                    int32_t low,
-                   int32_t high)
+                   int32_t high,
+                   uint32_t rwl)
 {
-  FILE *f                = NULL;
+  assert (func != NULL);
+  assert (func_name != NULL);
+  assert (low > 0);
+  assert (low <= high);
+
+  FILE *fin              = NULL;
+  FILE *fout             = NULL;
   int32_t i              = 0;
   int32_t j              = 0;
   int32_t const1_id      = 0;
@@ -133,11 +140,9 @@ s_arithmetic_test (int32_t (*func) (int32_t, int32_t),
   int32_t result         = 0;
   int32_t num_bits       = 0;
   int32_t max            = 0;
-  BtorExitCode exit_code = 0;
-  assert (func != NULL);
-  assert (func_name != NULL);
-  assert (low > 0);
-  assert (low <= high);
+  int32_t parse_res, parse_status;
+  char *parse_err;
+
   for (num_bits = low; num_bits <= high; num_bits++)
   {
     max = btor_util_pow_2 (num_bits - 1);
@@ -148,32 +153,37 @@ s_arithmetic_test (int32_t (*func) (int32_t, int32_t),
         result = func (i, j);
         if (result >= -max && result < max)
         {
-          f = fopen (BTOR_TEST_ARITHMETIC_TEMP_FILE_NAME, "w");
-          assert (f != NULL);
+          g_btor = boolector_new ();
+          boolector_set_opt (g_btor, BTOR_OPT_REWRITE_LEVEL, rwl);
+          if (g_rwreads)
+            boolector_set_opt (g_btor, BTOR_OPT_BETA_REDUCE_ALL, 1);
+
+          fin = fopen (BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME, "w");
+          assert (fin != NULL);
           if (i < 0)
           {
-            fprintf (f, "1 constd %d %d\n", num_bits, -i);
-            fprintf (f, "2 neg %d 1\n", num_bits);
+            fprintf (fin, "1 constd %d %d\n", num_bits, -i);
+            fprintf (fin, "2 neg %d 1\n", num_bits);
             const1_id = 2;
           }
           else
           {
-            fprintf (f, "1 constd %d %d\n", num_bits, i);
+            fprintf (fin, "1 constd %d %d\n", num_bits, i);
             const1_id = 1;
           }
           if (j < 0)
           {
-            fprintf (f, "%d constd %d %d\n", const1_id + 1, num_bits, -j);
+            fprintf (fin, "%d constd %d %d\n", const1_id + 1, num_bits, -j);
             fprintf (
-                f, "%d neg %d %d\n", const1_id + 2, num_bits, const1_id + 1);
+                fin, "%d neg %d %d\n", const1_id + 2, num_bits, const1_id + 1);
             const2_id = const1_id + 2;
           }
           else
           {
-            fprintf (f, "%d constd %d %d\n", const1_id + 1, num_bits, j);
+            fprintf (fin, "%d constd %d %d\n", const1_id + 1, num_bits, j);
             const2_id = const1_id + 1;
           }
-          fprintf (f,
+          fprintf (fin,
                    "%d %s %d %d %d\n",
                    const2_id + 1,
                    func_name,
@@ -182,22 +192,39 @@ s_arithmetic_test (int32_t (*func) (int32_t, int32_t),
                    const2_id);
           if (result < 0)
           {
-            fprintf (f, "%d constd %d %d\n", const2_id + 2, num_bits, -result);
             fprintf (
-                f, "%d neg %d %d\n", const2_id + 3, num_bits, const2_id + 2);
+                fin, "%d constd %d %d\n", const2_id + 2, num_bits, -result);
+            fprintf (
+                fin, "%d neg %d %d\n", const2_id + 3, num_bits, const2_id + 2);
             const3_id = const2_id + 3;
           }
           else
           {
-            fprintf (f, "%d constd %d %d\n", const2_id + 2, num_bits, result);
+            fprintf (fin, "%d constd %d %d\n", const2_id + 2, num_bits, result);
             const3_id = const2_id + 2;
           }
           fprintf (
-              f, "%d eq 1 %d %d\n", const3_id + 1, const2_id + 1, const3_id);
-          fprintf (f, "%d root 1 %d\n", const3_id + 2, const3_id + 1);
-          fclose (f);
-          exit_code = boolector_main (g_argc, g_argv);
-          assert (exit_code == BTOR_SAT_EXIT);
+              fin, "%d eq 1 %d %d\n", const3_id + 1, const2_id + 1, const3_id);
+          fprintf (fin, "%d root 1 %d\n", const3_id + 2, const3_id + 1);
+          fclose (fin);
+          fin = fopen (BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME, "r");
+          assert (fin != NULL);
+          fout = fopen (BTOR_TEST_ARITHMETIC_TEMP_OUTFILE_NAME, "w");
+          assert (fout != NULL);
+          parse_res =
+              boolector_parse_btor (g_btor,
+                                    fin,
+                                    BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME,
+                                    fout,
+                                    &parse_err,
+                                    &parse_status);
+          assert (parse_res != BOOLECTOR_PARSE_ERROR);
+          assert (boolector_sat (g_btor) == BOOLECTOR_SAT);
+          fclose (fin);
+          fclose (fout);
+          boolector_delete (g_btor);
+          assert (remove (BTOR_TEST_ARITHMETIC_TEMP_INFILE_NAME) == 0);
+          assert (remove (BTOR_TEST_ARITHMETIC_TEMP_OUTFILE_NAME) == 0);
         }
       }
     }
@@ -245,70 +272,90 @@ static void
 test_add_u_arithmetic (void)
 {
   u_arithmetic_test (
-      add, "add", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      add, "add", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  u_arithmetic_test (
+      add, "add", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_sub_u_arithmetic (void)
 {
   u_arithmetic_test (
-      sub, "sub", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      sub, "sub", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  u_arithmetic_test (
+      sub, "sub", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_mul_u_arithmetic (void)
 {
   u_arithmetic_test (
-      mul, "mul", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      mul, "mul", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  u_arithmetic_test (
+      mul, "mul", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_udiv_arithmetic (void)
 {
   u_arithmetic_test (
-      divide, "udiv", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      divide, "udiv", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  u_arithmetic_test (
+      divide, "udiv", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_urem_arithmetic (void)
 {
   u_arithmetic_test (
-      rem, "urem", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      rem, "urem", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  u_arithmetic_test (
+      rem, "urem", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_add_s_arithmetic (void)
 {
   s_arithmetic_test (
-      add, "add", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      add, "add", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  s_arithmetic_test (
+      add, "add", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_sub_s_arithmetic (void)
 {
   s_arithmetic_test (
-      sub, "sub", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      sub, "sub", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  s_arithmetic_test (
+      sub, "sub", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_mul_s_arithmetic (void)
 {
   s_arithmetic_test (
-      mul, "mul", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      mul, "mul", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  s_arithmetic_test (
+      mul, "mul", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_sdiv_arithmetic (void)
 {
   s_arithmetic_test (
-      divide, "sdiv", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      divide, "sdiv", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  s_arithmetic_test (
+      divide, "sdiv", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
 test_srem_arithmetic (void)
 {
   s_arithmetic_test (
-      rem, "srem", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH);
+      rem, "srem", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 1);
+  s_arithmetic_test (
+      rem, "srem", BTOR_TEST_ARITHMETIC_LOW, BTOR_TEST_ARITHMETIC_HIGH, 0);
 }
 
 static void
@@ -319,6 +366,7 @@ run_all_tests (int32_t argc, char **argv)
   BTOR_RUN_TEST (mul_u_arithmetic);
   BTOR_RUN_TEST (udiv_arithmetic);
   BTOR_RUN_TEST (urem_arithmetic);
+
   BTOR_RUN_TEST (add_s_arithmetic);
   BTOR_RUN_TEST (sub_s_arithmetic);
   BTOR_RUN_TEST (mul_s_arithmetic);
@@ -330,16 +378,10 @@ void
 run_arithmetic_tests (int32_t argc, char **argv)
 {
   run_all_tests (argc, argv);
-  g_argv[1] = "-rwl";
-  g_argv[2] = "0";
   run_all_tests (argc, argv);
 }
 
 void
 finish_arithmetic_tests (void)
 {
-  int32_t result = remove (BTOR_TEST_ARITHMETIC_TEMP_FILE_NAME);
-  assert (result == 0);
-  free (g_btor_str);
-  free (g_argv);
 }
