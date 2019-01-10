1f35c60d5b552878b36d84b06f42c1138b9182de
Andrew V. Jones <andrew.jones@vector.com>
2019-01-10 20:32:24 +0000

    Add documentation and patches for a Windows build (#15)
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

--- contrib/setup-lingeling.sh
+++ contrib/setup-lingeling.sh
@@ -12,5 +12,18 @@ LINGELING_DIR=${SETUP_DIR}/lingeling
 # Download and build Lingeling
 git clone --depth 1 https://github.com/arminbiere/lingeling.git ${LINGELING_DIR}
 cd ${LINGELING_DIR}
+case "$(uname -s)" in
+   CYGWIN*|MINGW32*|MSYS*)
+     revision=03b4860
+     echo ""
+     echo " *** WARNING: Building Lingeling on Windows relies on a specific"
+     echo " ***          revision (${revision})."
+     echo " ***          This version of Boolector may be built against an"
+     echo " ***          older version of Lingeling."
+     echo ""
+     git reset --hard "${revision}"
+     patch -p1 < ../../contrib/windows_patches/lingeling_"${revision}".patch
+     ;;
+esac
 ./configure.sh -fPIC
 make -j2
