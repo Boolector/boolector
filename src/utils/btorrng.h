120eeb413b0319c809d32df991bf8659a6d447f9
Aina Niemetz <aina.niemetz@gmail.com>
2019-05-16 13:15:31 -0700

    rng: Document header.

--- src/utils/btorrng.h
+++ src/utils/btorrng.h
@@ -26,14 +26,36 @@ struct BtorRNG
 };
 typedef struct BtorRNG BtorRNG;
 
+/**
+ * Initialize RNG with given seed. If compiled with GMP, this additionally
+ * initializes the state of the GMP RNG, thus calling this function is in
+ * that case mandatory.
+ */
 void btor_rng_init (BtorRNG* rng, uint32_t seed);
+
+/**
+ * Initialize given RNG clone when cloning.
+ * This does nothing when not compiled with GMP but must be called when cloning
+ * objects that maintain an RNG struct (and thus memcpy the state of this object
+ * to the cloned object when cloning) when compiled with GMP.
+ */
 void btor_rng_clone (BtorRNG* rng, BtorRNG* clone);
+
+/**
+ * Delete allocated data members of the given RNG.
+ * This does nothing when not compiled with GMP but must be called to free
+ * allocated GMP data members when compiled with GMP.
+ */
 void btor_rng_delete (BtorRNG* rng);
 
+/* Pick a random unsigned 32 bit integer. */
 uint32_t btor_rng_rand (BtorRNG* rng);
+/* Pick a random unsigned 32 bit integer between given bounds (inclusive). */
 uint32_t btor_rng_pick_rand (BtorRNG* rng, uint32_t from, uint32_t to);
+/* Pick a random double between given bounds (inclusive). */
 double btor_rng_pick_rand_dbl (BtorRNG* rng, double from, double to);
 
+/* Pick true with given probability (1000 = 100%). */
 bool btor_rng_pick_with_prob (BtorRNG* rng, uint32_t prob);
 
 #endif
