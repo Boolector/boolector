15f07d20817c413c8a635254d30ffb11fae945bd
Andrew V. Jones <andrew.jones@vector.com>
2019-07-03 12:00:30 +0100

    Reworking the Windows patch mechanism to not be dependant upon a specific revision. Patches are now applied against any revision, as long as they apply cleanly. Otherwise, compliation does not continue. (#35)
    Reworking the Windows patch mechanism to not be dependent upon a specific revision. Patches are now applied against any revision, as long as they apply cleanly. Otherwise, compilation does not continue.
    
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- contrib/setup-cadical.sh
+++ contrib/setup-cadical.sh
@@ -7,21 +7,22 @@ CADICAL_DIR=${DEPS_DIR}/cadical
 # Download and build CaDiCaL
 git clone --depth 1 https://github.com/arminbiere/cadical.git ${CADICAL_DIR}
 cd ${CADICAL_DIR}
-case "$(uname -s)" in
-   CYGWIN*|MINGW32*|MSYS*)
-     revision=58331fd
-     echo ""
-     echo " *** WARNING: Building CaDiCaL on Windows relies on a specific"
-     echo " ***          revision (${revision})."
-     echo " ***          This version of Boolector may be built against an"
-     echo " ***          older version of CaDiCaL."
-     echo ""
-     git reset --hard "${revision}"
-     patch -p1 < ../../contrib/windows_patches/cadical_"${revision}".patch
-     EXTRA_FLAGS="-q"
-     ;;
-esac
-CXXFLAGS="-fPIC" ./configure ${EXTRA_FLAGS}
+
+if is_windows; then
+  component="CaDiCaL"
+  last_patch_date="20190623"
+  test_apply_patch "${component}" "${last_patch_date}"
+  EXTRA_FLAGS="-q"
+  #
+  # CaDiCaL performs configure checks with -Werror, which fails on Windows as
+  # fPIC is not valid, so we set CXXFLAGS per-platform
+  #
+  export CXXFLAGS=""
+else
+  export CXXFLAGS="-fPIC"
+fi
+
+./configure ${EXTRA_FLAGS}
 make -j${NPROC}
 install_lib build/libcadical.a
 install_include src/ccadical.h
--- contrib/setup-lingeling.sh
+++ contrib/setup-lingeling.sh
@@ -7,19 +7,13 @@ LINGELING_DIR=${DEPS_DIR}/lingeling
 # Download and build Lingeling
 git clone --depth 1 https://github.com/arminbiere/lingeling.git ${LINGELING_DIR}
 cd ${LINGELING_DIR}
-case "$(uname -s)" in
-   CYGWIN*|MINGW32*|MSYS*)
-     revision=03b4860
-     echo ""
-     echo " *** WARNING: Building Lingeling on Windows relies on a specific"
-     echo " ***          revision (${revision})."
-     echo " ***          This version of Boolector may be built against an"
-     echo " ***          older version of Lingeling."
-     echo ""
-     git reset --hard "${revision}"
-     patch -p1 < ../../contrib/windows_patches/lingeling_"${revision}".patch
-     ;;
-esac
+
+if is_windows; then
+  component="Lingeling"
+  last_patch_date="20190110"
+  test_apply_patch "${component}" "${last_patch_date}"
+fi
+
 ./configure.sh -fPIC
 make -j${NPROC}
 install_lib liblgl.a
