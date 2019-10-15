043c048c2c0b6fa7864bd3b30fbe162873fe61e8
Aina Niemetz <aina.niemetz@gmail.com>
2019-10-15 16:54:23 -0700

    google test: Migrate testsat.c.
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ test/new_test/test_satmgr.cpp
@@ -0,0 +1,44 @@
+/*  Boolector: Satisfiability Modulo Theories (SMT) solver.
+ *
+ *  Copyright (C) 2007-2010 Robert Daniel Brummayer.
+ *  Copyright (C) 2007-2012 Armin Biere.
+ *  Copyright (C) 2014-2019 Aina Niemetz.
+ *
+ *  This file is part of Boolector.
+ *  See COPYING for more information on using this software.
+ */
+
+#include "test.h"
+
+extern "C" {
+#include "btoraig.h"
+#include "dumper/btordumpaig.h"
+}
+
+class TestSatMgr : public TestBtor
+{
+ protected:
+  void SetUp () override
+  {
+    TestBtor::SetUp ();
+    d_smgr = btor_sat_mgr_new (d_btor);
+  }
+  void TearDown () override
+  {
+    btor_sat_mgr_delete (d_smgr);
+    TestBtor::TearDown ();
+  }
+  BtorSATMgr *d_smgr = nullptr;
+};
+
+TEST_F (TestSatMgr, new_delete) {}
+
+TEST_F (TestSatMgr, next_cnf_id)
+{
+  btor_sat_enable_solver (d_smgr);
+  btor_sat_init (d_smgr);
+  ASSERT_EQ (btor_sat_mgr_next_cnf_id (d_smgr), 2);
+  ASSERT_EQ (btor_sat_mgr_next_cnf_id (d_smgr), 3);
+  ASSERT_EQ (btor_sat_mgr_next_cnf_id (d_smgr), 4);
+  btor_sat_reset (d_smgr);
+}
