ba0fd1b90c4fcb7470919b8e62523dcbbe8255e0
Aina Niemetz <aina.niemetz@gmail.com>
2019-11-01 11:57:25 -0700

    Update copyright.

--- src/btordbg.c
+++ src/btordbg.c
@@ -3,7 +3,7 @@
  *  Copyright (C) 2007-2009 Robert Daniel Brummayer.
  *  Copyright (C) 2007-2013 Armin Biere.
  *  Copyright (C) 2012-2017 Mathias Preiner.
- *  Copyright (C) 2012-2017 Aina Niemetz.
+ *  Copyright (C) 2012-2019 Aina Niemetz.
  *
  *  This file is part of Boolector.
  *  See COPYING for more information on using this software.
