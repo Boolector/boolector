8efce4575f11744f9452d590a2cf83d57b86f8c0
Felix <femigr@gmail.com>
2020-05-20 17:02:31 +0200

    Fixed handling of paths containing spaces by some bash files
    Signed-off-by: Felix <femigr@gmail.com>

--- contrib/setup-btor2tools.sh
+++ contrib/setup-btor2tools.sh
@@ -8,7 +8,7 @@ BTOR2TOOLS_DIR=${DEPS_DIR}/btor2tools
 COMMIT_ID="1df768d75adfb13a8f922f5ffdd1d58e80cb1cc2"
 
 download_github "boolector/btor2tools" "$COMMIT_ID" "$BTOR2TOOLS_DIR"
-cd ${BTOR2TOOLS_DIR}
+cd "${BTOR2TOOLS_DIR}"
 
 if is_windows; then
   component="Btor2Tools"
