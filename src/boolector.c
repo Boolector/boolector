e7baeba44a91def347cc4ddd478e0d7c5d2ea660
Andrew V. Jones <andrew.jones@vector.com>
2020-05-26 11:47:28 +0100

    Ensuring copyright years in '-c' is up-to-date
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

--- src/boolector.c
+++ src/boolector.c
@@ -4914,7 +4914,7 @@ boolector_copyright (Btor *btor)
   return "This software is\n"
          "Copyright (c) 2007-2009 Robert Brummayer\n"
          "Copyright (c) 2007-2018 Armin Biere\n"
-         "Copyright (c) 2012-2019 Aina Niemetz, Mathias Preiner\n"
+         "Copyright (c) 2012-2020 Aina Niemetz, Mathias Preiner\n"
 #ifdef BTOR_USE_LINGELING
          "\n"
          "This software is linked against Lingeling\n"
