06477d65ef5e7f6a28640f3f439115838a2ddec4
Florian Lonsing <lonsing@cs.stanford.edu>
2020-06-19 17:45:58 -0700

    update scripts
    - updated scripts copied from master, commit 74c9e8822d1d4e5f8c9e0df68e02309d1505e280
    
    Signed-off-by: Florian Lonsing <lonsing@cs.stanford.edu>

--- contrib/setup-btor2tools.sh
+++ contrib/setup-btor2tools.sh
@@ -5,11 +5,9 @@ set -e -o pipefail
 source "$(dirname "$0")/setup-utils.sh"
 
 BTOR2TOOLS_DIR=${DEPS_DIR}/btor2tools
+COMMIT_ID="1df768d75adfb13a8f922f5ffdd1d58e80cb1cc2"
 
-rm -rf ${BTOR2TOOLS_DIR}
-
-# Download and build btor2tools
-git clone --depth 1 https://github.com/Boolector/btor2tools.git ${BTOR2TOOLS_DIR}
+download_github "boolector/btor2tools" "$COMMIT_ID" "$BTOR2TOOLS_DIR"
 cd ${BTOR2TOOLS_DIR}
 
 if is_windows; then
