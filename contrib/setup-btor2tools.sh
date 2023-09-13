be1f827e911666934e8b84479673a7c5d9dda2eb
Mathias Preiner <mathias.preiner@gmail.com>
2023-09-13 09:36:15 -0700

    contrib: Update Btor2Tools to most recent version.
    Makes the Btor2Tools Windows patch obsolete.

--- contrib/setup-btor2tools.sh
+++ contrib/setup-btor2tools.sh
@@ -13,18 +13,14 @@ set -e -o pipefail
 source "$(dirname "$0")/setup-utils.sh"
 
 BTOR2TOOLS_DIR=${DEPS_DIR}/btor2tools
-COMMIT_ID="1df768d75adfb13a8f922f5ffdd1d58e80cb1cc2"
+COMMIT_ID="037f1fa88fb439dca6f648ad48a3463256d69d8b"
 
 download_github "boolector/btor2tools" "$COMMIT_ID" "$BTOR2TOOLS_DIR"
 cd "${BTOR2TOOLS_DIR}"
 
-if is_windows; then
-  component="Btor2Tools"
-  last_patch_date="20190110"
-  test_apply_patch "${component}" "${last_patch_date}"
-fi
-
-./configure.sh -fPIC
+CFLAGS="-fPIC" ./configure.sh --static
+pushd build
 make -j${NPROC}
-install_lib build/libbtor2parser.a
+popd
+install_lib build/lib/libbtor2parser.a
 install_include src/btor2parser/btor2parser.h
